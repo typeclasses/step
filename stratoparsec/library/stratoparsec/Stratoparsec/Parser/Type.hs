cd65c83b8c6d2d14d5663cf5b650c6676f02afa1
Chris Martin <ch.martin@gmail.com>
2022-06-20 21:43:26 -0600

    what if we just don't do backtracking

--- stratoparsec/library/stratoparsec/Stratoparsec/Parser/Type.hs
+++ stratoparsec/library/stratoparsec/Stratoparsec/Parser/Type.hs
@@ -3,21 +3,19 @@ module Stratoparsec.Parser.Type where
 import Stratoparsec.Context (Context)
 import Stratoparsec.BufferedStream.Type (BufferedStream)
 
-import ListT
-
 type Parser :: (Type -> Type) -> Type -> Type -> Type -> Type
 
 newtype Parser m chunk buffer a =
   Parser
     (Context ->
-        (StateT (BufferedStream m chunk buffer) (ListT m) a)
+        (StateT (BufferedStream m chunk buffer) m a)
     )
 
 deriving stock
     instance Monad m => Functor (Parser m chunk buffer)
 
-deriving via ReaderT Context (StateT (BufferedStream m chunk buffer) (ListT m))
+deriving via ReaderT Context (StateT (BufferedStream m chunk buffer) m)
     instance Monad m => Applicative (Parser m chunk buffer)
 
-deriving via ReaderT Context (StateT (BufferedStream m chunk buffer) (ListT m))
+deriving via ReaderT Context (StateT (BufferedStream m chunk buffer) m)
     instance Monad m => Monad (Parser m chunk buffer)
