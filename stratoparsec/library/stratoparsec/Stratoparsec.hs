a761550b98e03d69de3fa1323b43a4955bd88066
Chris Martin <ch.martin@gmail.com>
2022-06-23 13:46:13 -0600

    mono-traversable instead of TextLike

--- stratoparsec/library/stratoparsec/Stratoparsec.hs
+++ stratoparsec/library/stratoparsec/Stratoparsec.hs
@@ -4,57 +4,14 @@ import Char (Char)
 import Word (Word8)
 import ListT (ListT)
 import Seq (Seq)
+import Mono (MonoFoldable)
 
 import qualified ByteString as BS
 import qualified LByteString as LBS
 import qualified Text as T
 import qualified LText as LT
 import qualified Seq
-
-class TextLike text where
-    type TextLikeChar text :: Type
-    type TextLikeLazy text :: Type
-    type TextLikeBuffer text :: Type
-    textLikeToBuffer :: text -> TextLikeBuffer text
-    textLikeBufferConcat :: TextLikeBuffer text -> TextLikeBuffer text -> TextLikeBuffer text
-    textLikeBufferToLazy :: TextLikeBuffer text -> TextLikeLazy text
-    textLikeBufferSize :: TextLikeBuffer text -> Natural
-
-instance TextLike T.Text
-  where
-    type TextLikeChar T.Text = Char
-    type TextLikeLazy T.Text = LT.Text
-    type TextLikeBuffer T.Text = TextBuffer
-    textLikeToBuffer x =
-        TextBuffer{
-            textBufferTexts = Seq.singleton x,
-            textBufferLength = fromIntegral (T.length x)
-        }
-    textLikeBufferConcat a b =
-        TextBuffer{
-            textBufferTexts = textBufferTexts a <> textBufferTexts b,
-            textBufferLength = textBufferLength a + textBufferLength b
-        }
-    textLikeBufferToLazy x = LT.fromChunks (toList (textBufferTexts x))
-    textLikeBufferSize = textBufferLength
-
-instance TextLike BS.ByteString
-  where
-    type TextLikeChar BS.ByteString = Word8
-    type TextLikeLazy BS.ByteString = LBS.ByteString
-    type TextLikeBuffer BS.ByteString = ByteStringBuffer
-    textLikeToBuffer x =
-        ByteStringBuffer{
-            byteStringBufferTexts = Seq.singleton x,
-            byteStringBufferLength = fromIntegral (BS.length x)
-        }
-    textLikeBufferConcat  a b =
-        ByteStringBuffer{
-            byteStringBufferTexts = byteStringBufferTexts a <> byteStringBufferTexts b,
-            byteStringBufferLength = byteStringBufferLength a + byteStringBufferLength b
-        }
-    textLikeBufferToLazy x = LBS.fromChunks (toList (byteStringBufferTexts x))
-    textLikeBufferSize = byteStringBufferLength
+import qualified Mono
 
 type BufferedStream :: (Type -> Type) -> Type -> Type -> Type
 data BufferedStream m chunk buffer =
@@ -63,22 +20,16 @@ data BufferedStream m chunk buffer =
     , bufferedStreamPending :: ListT m chunk
     }
 
-data ByteStringBuffer =
-  ByteStringBuffer
-    { byteStringBufferTexts :: Seq ByteString
-    , byteStringBufferLength :: Natural
-    }
+textLikeSingletonBuffer :: MonoFoldable text => text -> Buffer text
+textLikeSingletonBuffer x =
+    Buffer{ bufferChunks = Seq.singleton x, bufferSize = fromIntegral $ Mono.olength x }
 
-data TextBuffer =
-  TextBuffer
-    { textBufferTexts :: Seq Text
-    , textBufferLength :: Natural
-    }
+data Buffer a = Buffer{ bufferChunks :: Seq a, bufferSize :: Natural }
 
 -- | Force the input until at least @n@ characters of input are buffered or the end of input is reached.
 bufferCharacters :: Natural
-    -> BufferedStream m text (TextLikeBuffer text)
-    -> m (BufferedStream m text (TextLikeBuffer text))
+    -> BufferedStream m text (Mono.Element text)
+    -> m (BufferedStream m text (Mono.Element text))
 bufferCharacters n s = _
 
 -- bufferAtLeast :: forall m text. (Monad m, TextLike text) => Natural -> Parser m text ()
