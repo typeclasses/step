fac70e089d0e9efa4b0855677b2e7cee30c858b9
Chris Martin <ch.martin@gmail.com>
2022-06-26 12:37:16 -0600

    test dedent 1

--- stratoparsec/test/test-stratoparsec/Main.hs
+++ stratoparsec/test/test-stratoparsec/Main.hs
@@ -17,40 +17,42 @@ import Test.Hspec.Hedgehog
 
 main :: IO ()
 main = hspec do
+    documentParsing
 
-    describe "Document parsing" do
+documentParsing :: SpecWith ()
+documentParsing = describe "Document parsing" do
 
-        describe "p = char, char, char" do
-            let p = (,,) <$> Doc.char <*> Doc.char <*> Doc.char
-            specify "(p <* end) parses \"abc\"" $ hedgehog do
-                input <- forAll (genChunks "abc")
-                x <- Doc.parseOnly Doc.defaultErrorOptions (p <* Doc.end) (ListT.select input)
-                x === Right ('a', 'b', 'c')
-            specify "p parses \"abcd\"" $ hedgehog do
-                input <- forAll (genChunks "abcd")
-                x <- Doc.parseOnly Doc.defaultErrorOptions p (ListT.select input)
-                x === Right ('a', 'b', 'c')
-            specify "(p <* end) fails on \"abcd\"" $ hedgehog do
-                input <- forAll (genChunks "abcd")
-                x <- Doc.parseOnly Doc.defaultErrorOptions (p <* Doc.end) (ListT.select input)
-                x === Left (Doc.Error [])
+    describe "p = char, char, char" do
+        let p = (,,) <$> Doc.char <*> Doc.char <*> Doc.char
+        specify "(p <* end) parses \"abc\"" $ hedgehog do
+            input <- forAll (genChunks "abc")
+            x <- Doc.parseOnly Doc.defaultErrorOptions (p <* Doc.end) (ListT.select input)
+            x === Right ('a', 'b', 'c')
+        specify "p parses \"abcd\"" $ hedgehog do
+            input <- forAll (genChunks "abcd")
+            x <- Doc.parseOnly Doc.defaultErrorOptions p (ListT.select input)
+            x === Right ('a', 'b', 'c')
+        specify "(p <* end) fails on \"abcd\"" $ hedgehog do
+            input <- forAll (genChunks "abcd")
+            x <- Doc.parseOnly Doc.defaultErrorOptions (p <* Doc.end) (ListT.select input)
+            x === Left (Doc.Error [])
 
-        describe "p = contextualize \"Digit\" (satisfy isDigit)" do
-            let p = Doc.contextualize "Digit" (Doc.satisfy Char.isDigit)
-            specify "p parses \"2\"" $ hedgehog do
-                x <- Doc.parseOnly Doc.defaultErrorOptions p (ListT.select ["2"])
-                x === Right '2'
-            specify "p fails on \"a\"" $ hedgehog do
-                x <- Doc.parseOnly Doc.defaultErrorOptions p (ListT.select ["a"])
-                x === Left (Doc.Error ["Digit"])
+    describe "p = contextualize \"Digit\" (satisfy isDigit)" do
+        let p = Doc.contextualize "Digit" (Doc.satisfy Char.isDigit)
+        specify "p parses \"2\"" $ hedgehog do
+            x <- Doc.parseOnly Doc.defaultErrorOptions p (ListT.select ["2"])
+            x === Right '2'
+        specify "p fails on \"a\"" $ hedgehog do
+            x <- Doc.parseOnly Doc.defaultErrorOptions p (ListT.select ["a"])
+            x === Left (Doc.Error ["Digit"])
 
-        describe "p = text \"abc\"" do
-            let p = Doc.text "abc"
-            specify "p parses \"abc\" and \"abcd\"" $ hedgehog do
-                input <- forAll (Gen.element ["abc", "abcd"] >>= genChunks)
-                x <- Doc.parseOnly Doc.defaultErrorOptions p (ListT.select input)
-                x === Right ()
-            specify "p fails on any input that does not start with abc" $ hedgehog do
-                input <- forAll (Gen.element ["", "ab", "bc"] >>= genChunks)
-                x <- Doc.parseOnly Doc.defaultErrorOptions p (ListT.select input)
-                x === Left (Doc.Error [])
+    describe "p = text \"abc\"" do
+        let p = Doc.text "abc"
+        specify "p parses \"abc\" and \"abcd\"" $ hedgehog do
+            input <- forAll (Gen.element ["abc", "abcd"] >>= genChunks)
+            x <- Doc.parseOnly Doc.defaultErrorOptions p (ListT.select input)
+            x === Right ()
+        specify "p fails on any input that does not start with abc" $ hedgehog do
+            input <- forAll (Gen.element ["", "ab", "bc"] >>= genChunks)
+            x <- Doc.parseOnly Doc.defaultErrorOptions p (ListT.select input)
+            x === Left (Doc.Error [])
