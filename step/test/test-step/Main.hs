39f50c0d8682edfd0b957315ba0e3ba111b0b38b
Chris Martin <ch.martin@gmail.com>
2022-06-30 17:23:18 -0600

    finish making test compile

--- step/test/test-step/Main.hs
+++ step/test/test-step/Main.hs
@@ -2,28 +2,10 @@ module Main (main) where
 
 import Step.Internal.Prelude
 
-import Hedgehog
-
-import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
-
-import qualified ListT
-import qualified ListLike
-import qualified Char
-
-import qualified Step.Document.Parser as Doc
-import qualified Step.Document.Prelude as Doc
-
-import Step.Test.InputChunking (genChunks)
-
 import Test.Hspec
-import Test.Hspec.Hedgehog
 
-import Text (Text)
-
-import Loc (loc)
-import qualified SpanOrLoc
+import qualified Step.Spec.DocumentParsing as DocumentParsing
 
 main :: IO ()
 main = hspec do
-    documentParsingSpec
+    DocumentParsing.spec
--- step/test/test-step/Step/Spec/DocumentParsing.hs
+++ step/test/test-step/Step/Spec/DocumentParsing.hs
@@ -1,6 +1,31 @@
+module Step.Spec.DocumentParsing where
 
-documentParsing :: SpecWith ()
-documentParsing = describe "Document parsing" do
+import Step.Internal.Prelude
+
+import Hedgehog
+
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+import qualified ListT
+import qualified ListLike
+import qualified Char
+
+import qualified Step.Document.Parser as Doc
+import qualified Step.Document.Prelude as Doc
+
+import Step.Test.InputChunking (genChunks)
+
+import Test.Hspec
+import Test.Hspec.Hedgehog
+
+import Text (Text)
+
+import Loc (loc)
+import qualified SpanOrLoc
+
+spec :: SpecWith ()
+spec = describe "Document parsing" do
 
     describe "p = char, char, char" do
         let c = Doc.require Doc.char
