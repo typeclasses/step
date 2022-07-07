a74e5a58acee3eecd3ec4e3138f2e5efcebd43ad
Chris Martin <ch.martin@gmail.com>
2022-07-06 21:07:08 -0600

    haddock lift instances

--- step/library/step/Step/Action/Lift.hs
+++ step/library/step/Step/Action/Lift.hs
@@ -20,36 +20,59 @@ actionLiftTo = actionLift @k1 @k2
 sureToAny :: Functor m => Sure config cursor error m a -> Any config cursor error m a
 sureToAny (Sure p) = Any (\c -> p c <&> Right)
 
--- Everything trivially lifts to itself
+-- | Everything trivially lifts to itself
+instance ActionLift Any Any where actionLift = coerce
 
-instance ActionLift Any        Any        where actionLift = coerce
-instance ActionLift Query      Query      where actionLift = coerce
-instance ActionLift Move       Move       where actionLift = coerce
-instance ActionLift Atom       Atom       where actionLift = coerce
+-- | Everything trivially lifts to itself
+instance ActionLift Query Query where actionLift = coerce
+
+-- | Everything trivially lifts to itself
+instance ActionLift Move Move where actionLift = coerce
+
+-- | Everything trivially lifts to itself
+instance ActionLift Atom Atom where actionLift = coerce
+
+-- | Everything trivially lifts to itself
 instance ActionLift AtomicMove AtomicMove where actionLift = coerce
-instance ActionLift Sure       Sure       where actionLift = coerce
-instance ActionLift SureQuery  SureQuery  where actionLift = coerce
 
--- Everything lifts to Any
+-- | Everything trivially lifts to itself
+instance ActionLift Sure Sure where actionLift = coerce
 
-instance ActionLift Query      Any  where actionLift = coerce
-instance ActionLift Move       Any  where actionLift = coerce
-instance ActionLift AtomicMove Any  where actionLift = coerce
-instance ActionLift Sure       Any  where actionLift = Coerce.from @Any . sureToAny . Coerce.to @Sure
-instance ActionLift SureQuery  Any  where actionLift = Coerce.from @Any . sureToAny . Coerce.to @Sure
-instance ActionLift Atom       Any  where actionLift = coerce
+-- | Everything trivially lifts to itself
+instance ActionLift SureQuery SureQuery where actionLift = coerce
 
--- AtomicMove lifts to either Atom or Move
+-- | Everything lifts to Any
+instance ActionLift Query Any where actionLift = coerce
 
+-- | Everything lifts to Any
+instance ActionLift Move Any where actionLift = coerce
+
+-- | Everything lifts to Any
+instance ActionLift AtomicMove Any where actionLift = coerce
+
+-- | Everything lifts to Any
+instance ActionLift Sure Any where actionLift = Coerce.from @Any . sureToAny . Coerce.to @Sure
+
+-- | Everything lifts to Any
+instance ActionLift SureQuery Any where actionLift = Coerce.from @Any . sureToAny . Coerce.to @Sure
+
+-- | Everything lifts to Any
+instance ActionLift Atom Any where actionLift = coerce
+
+-- | AtomicMove gets its name from the fact that it has the properties of both Atom and Move
 instance ActionLift AtomicMove Move where actionLift = coerce
+
+-- | AtomicMove gets its name from the fact that it has the properties of both Atom and Move
 instance ActionLift AtomicMove Atom where actionLift = coerce
 
--- SureQuery lifts to either Sure or Query
+-- | SureQuery gets its name from the fact that it has the properties of both Sure and Query
+instance ActionLift SureQuery Sure where actionLift = coerce
 
-instance ActionLift SureQuery Sure  where actionLift = coerce
+-- | SureQuery gets its name from the fact that it has the properties of both Sure and Query
 instance ActionLift SureQuery Query where actionLift = Coerce.from @Any . sureToAny . Coerce.to @Sure
 
--- Queries and Sure actions are trivially atomic
-
+-- | A Query is trivially atomic because it never moves the cursor, therefore it cannot move and fail
 instance ActionLift Query Atom where actionLift = coerce
-instance ActionLift Sure  Atom where actionLift = Coerce.from @Any . sureToAny . Coerce.to @Sure
+
+-- | A Sure action is trivially atomic because it never fails, therefore it cannot move and fail
+instance ActionLift Sure Atom where actionLift = Coerce.from @Any . sureToAny . Coerce.to @Sure
