f31c15d68eaca1108556ce09da147cc6849abcd4
Chris Martin <ch.martin@gmail.com>
2022-07-07 17:41:59 -0600

    add subtyping diagram to Subtyping module doc

--- step/library/step/Step/Action/Subtyping.hs
+++ step/library/step/Step/Action/Subtyping.hs
@@ -1,7 +1,13 @@
 module Step.Action.Subtyping
   (
-    Is,
+
     cast,
+
+    -- $graph
+
+    Is,
+    -- The class's method is not exported because there is no reason to define new instances of the class outside of this module.
+
   )
   where
 
@@ -11,9 +17,18 @@ import Step.Action.Kinds
 
 import qualified Step.Action.Coerce as Coerce
 
+
+-- $graph
+--
+-- Arrows in the graph below indicate permitted casts.
+--
+-- ![Action subtyping graph](graphics/action-subtyping.svg)
+
+
 class Is (k1 :: ActionKind) (k2 :: ActionKind)
   where
     cast' :: Monad m => k1 config cursor error m a -> k2 config cursor error m a
+
 
 cast :: forall k2 k1 config cursor error m a.
     Monad m =>
@@ -22,11 +37,17 @@ cast :: forall k2 k1 config cursor error m a.
     -> k2 config cursor error m a
 cast = cast' @k1 @k2
 
-sureToAny :: Functor m => Sure config cursor error m a -> Any config cursor error m a
+
+sureToAny :: Functor m =>
+    Sure config cursor error m a
+    -> Any config cursor error m a
+
 sureToAny (Sure p) = Any (\c -> p c <&> Right)
 
+-- sureToAny is for defining instances, below
 
---- Identity
+
+-- Identity
 
 -- | Everything trivially lifts to itself
 instance Is Any Any where cast' = coerce
@@ -50,7 +71,7 @@ instance Is Sure Sure where cast' = coerce
 instance Is SureQuery SureQuery where cast' = coerce
 
 
---- Any supertypes everything else
+-- Any supertypes everything else
 
 -- | Everything lifts to Any
 instance Is Query Any where cast' = coerce
@@ -71,7 +92,7 @@ instance Is SureQuery Any where cast' = Coerce.from @Any . sureToAny . Coerce.to
 instance Is Atom Any where cast' = coerce
 
 
---- Atom + Move = AtomicMove
+-- Atom + Move = AtomicMove
 
 -- | AtomicMove gets its name from the fact that it has the properties of both Atom and Move
 instance Is AtomicMove Move where cast' = coerce
@@ -80,7 +101,7 @@ instance Is AtomicMove Move where cast' = coerce
 instance Is AtomicMove Atom where cast' = coerce
 
 
---- Sure + Query = SureQuery
+-- Sure + Query = SureQuery
 
 -- | SureQuery gets its name from the fact that it has the properties of both Sure and Query
 instance Is SureQuery Sure where cast' = coerce
@@ -89,7 +110,7 @@ instance Is SureQuery Sure where cast' = coerce
 instance Is SureQuery Query where cast' = Coerce.from @Any . sureToAny . Coerce.to @Sure
 
 
---- Trivial subtypes of Atom
+-- Trivial subtypes of Atom
 
 -- | A Query is trivially atomic because it never moves the cursor, therefore it cannot move and fail
 instance Is Query Atom where cast' = coerce
