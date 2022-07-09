415a22f71f9a89a35d673cb4cf69c75a78fd55e7
Chris Martin <ch.martin@gmail.com>
2022-07-09 00:38:44 -0600

    Do fixities

--- step/library/step/Step/Document/Do.hs
+++ step/library/step/Step/Document/Do.hs
@@ -22,23 +22,28 @@ join :: (ActionJoin k1 k2, Monad m,
     -> Parser text (k1 :> k2) m a
 join = Parser . actionJoin . (\(Parser a) -> a) . fmap (\(Parser a) -> a)
 
+infixl 1 >>=
 (>>=) ::
     Monad m => IsAction k1 => IsAction k2 => IsAction k3 => ActionJoin k1 k2 => k1 :> k2 ~ k3 =>
     Parser text k1 m a -> (a -> Parser text k2 m b) -> Parser text k3 m b
 x >>= f = join (fmap f x)
 
+infixl 4 <*
 (<*) ::
     Monad m => IsAction k1 => IsAction k2 => IsAction k3 => ActionJoin k1 k2 => k1 :> k2 ~ k3 =>
     Parser text k1 m a -> Parser text k2 m b -> Parser text k3 m a
 a <* b = join (fmap (\x -> fmap (\_ -> x) b) a)
 
+infixl 4 *>
 (*>) ::
     Monad m => IsAction k1 => IsAction k2 => IsAction k3 => ActionJoin k1 k2 => k1 :> k2 ~ k3 =>
     Parser text k1 m a -> Parser text k2 m b -> Parser text k3 m b
 a *> b = join (fmap (\_ -> b) a)
 
+infixl 1 >>
 (>>) = (*>)
 
+infixl 4 <*>
 (<*>) ::
     Monad m => IsAction k1 => IsAction k2 => IsAction k3 => ActionJoin k1 k2 => k1 :> k2 ~ k3 =>
     Parser text k1 m (a -> b) -> Parser text k2 m a -> Parser text k3 m b
