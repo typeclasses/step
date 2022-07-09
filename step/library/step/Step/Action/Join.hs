f8774de2d4fedc41803544103ba9b7e2d6e41bc7
Chris Martin <ch.martin@gmail.com>
2022-07-09 02:26:23 -0600

    finish the ActionJoin class

--- step/library/step/Step/Action/Join.hs
+++ step/library/step/Step/Action/Join.hs
@@ -16,40 +16,58 @@ class (FunctorialAction k1, FunctorialAction k2, FunctorialAction (k1 >> k2)) =>
   where
     actionJoin :: Monad m => k1 config cursor error m (k2 config cursor error m a) -> (k1 >> k2) config cursor error m a
 
--- todo: all 49 instances
-
-instance ActionJoin Move       Move       where actionJoin = Coerced.join
-instance ActionJoin AtomicMove AtomicMove where actionJoin = Coerced.join
 instance ActionJoin Any        Any        where actionJoin = Coerced.join
-instance ActionJoin Query      Query      where actionJoin = Coerced.join
-instance ActionJoin Atom       Atom       where actionJoin = Coerced.join
-instance ActionJoin Sure       Sure       where actionJoin = Coerced.join
-instance ActionJoin SureQuery  SureQuery  where actionJoin = Coerced.join
-
-instance ActionJoin AtomicMove Move       where actionJoin = Coerced.join
-instance ActionJoin Move       AtomicMove where actionJoin = Coerced.join
-instance ActionJoin AtomicMove Any        where actionJoin = Coerced.join
-instance ActionJoin Any        AtomicMove where actionJoin = Coerced.join
-instance ActionJoin Move       Any        where actionJoin = Coerced.join
-instance ActionJoin Any        Move       where actionJoin = Coerced.join
-instance ActionJoin Move       Atom       where actionJoin = Coerced.join
-instance ActionJoin Atom       Move       where actionJoin = Coerced.join
-instance ActionJoin Move       Query      where actionJoin = Coerced.join
-instance ActionJoin Query      Move       where actionJoin = Coerced.join
-
-instance ActionJoin Any        SureQuery  where actionJoin = Coerced.join
-instance ActionJoin SureQuery  Any        where actionJoin = Coerced.join
-instance ActionJoin Query      SureQuery  where actionJoin = Coerced.join
-instance ActionJoin SureQuery  Query      where actionJoin = Coerced.join
-instance ActionJoin Move       SureQuery  where actionJoin = Coerced.join
-instance ActionJoin SureQuery  Move       where actionJoin = Coerced.join
-instance ActionJoin Atom       SureQuery  where actionJoin = Coerced.join
-instance ActionJoin SureQuery  Atom       where actionJoin = Coerced.join
-instance ActionJoin AtomicMove SureQuery  where actionJoin = Coerced.join
-instance ActionJoin SureQuery  AtomicMove where actionJoin = Coerced.join
-instance ActionJoin Sure       SureQuery  where actionJoin = Coerced.join
-instance ActionJoin SureQuery  Sure       where actionJoin = Coerced.join
-instance ActionJoin Atom       Any        where actionJoin = Coerced.join
 instance ActionJoin Any        Atom       where actionJoin = Coerced.join
+instance ActionJoin Any        AtomicMove where actionJoin = Coerced.join
+instance ActionJoin Any        Move       where actionJoin = Coerced.join
+instance ActionJoin Any        Query      where actionJoin = Coerced.join
+instance ActionJoin Any        SureQuery  where actionJoin = Coerced.join
+instance ActionJoin Any        Sure       where actionJoin = Coerced.join
+
+instance ActionJoin Atom       Any        where actionJoin = Coerced.join
+instance ActionJoin Atom       Atom       where actionJoin = Coerced.join
+instance ActionJoin Atom       AtomicMove where actionJoin = Coerced.join
+instance ActionJoin Atom       Move       where actionJoin = Coerced.join
+instance ActionJoin Atom       Query      where actionJoin = Coerced.join
+instance ActionJoin Atom       SureQuery  where actionJoin = Coerced.join
+instance ActionJoin Atom       Sure       where actionJoin = Coerced.join
+
+instance ActionJoin AtomicMove Any        where actionJoin = Coerced.join
+instance ActionJoin AtomicMove Atom       where actionJoin = Coerced.join
+instance ActionJoin AtomicMove AtomicMove where actionJoin = Coerced.join
+instance ActionJoin AtomicMove Move       where actionJoin = Coerced.join
+instance ActionJoin AtomicMove Query      where actionJoin = Coerced.join
+instance ActionJoin AtomicMove SureQuery  where actionJoin = Coerced.join
 instance ActionJoin AtomicMove Sure       where actionJoin = Coerced.join
+
+instance ActionJoin Move       Any        where actionJoin = Coerced.join
+instance ActionJoin Move       Atom       where actionJoin = Coerced.join
+instance ActionJoin Move       AtomicMove where actionJoin = Coerced.join
+instance ActionJoin Move       Move       where actionJoin = Coerced.join
+instance ActionJoin Move       Query      where actionJoin = Coerced.join
+instance ActionJoin Move       SureQuery  where actionJoin = Coerced.join
+instance ActionJoin Move       Sure       where actionJoin = Coerced.join
+
+instance ActionJoin Query      Any        where actionJoin = Coerced.join
+instance ActionJoin Query      Atom       where actionJoin = Coerced.join
+instance ActionJoin Query      AtomicMove where actionJoin = Coerced.join
+instance ActionJoin Query      Move       where actionJoin = Coerced.join
+instance ActionJoin Query      Query      where actionJoin = Coerced.join
+instance ActionJoin Query      SureQuery  where actionJoin = Coerced.join
+instance ActionJoin Query      Sure       where actionJoin = Coerced.join
+
+instance ActionJoin Sure       Any        where actionJoin = Coerced.join
+instance ActionJoin Sure       Atom       where actionJoin = Coerced.join
 instance ActionJoin Sure       AtomicMove where actionJoin = Coerced.join
+instance ActionJoin Sure       Move       where actionJoin = Coerced.join
+instance ActionJoin Sure       Query      where actionJoin = Coerced.join
+instance ActionJoin Sure       SureQuery  where actionJoin = Coerced.join
+instance ActionJoin Sure       Sure       where actionJoin = Coerced.join
+
+instance ActionJoin SureQuery  Any        where actionJoin = Coerced.join
+instance ActionJoin SureQuery  Atom       where actionJoin = Coerced.join
+instance ActionJoin SureQuery  AtomicMove where actionJoin = Coerced.join
+instance ActionJoin SureQuery  Move       where actionJoin = Coerced.join
+instance ActionJoin SureQuery  Query      where actionJoin = Coerced.join
+instance ActionJoin SureQuery  Sure       where actionJoin = Coerced.join
+instance ActionJoin SureQuery  SureQuery  where actionJoin = Coerced.join
