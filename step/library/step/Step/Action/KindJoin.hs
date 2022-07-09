8b6c720aaab2d4421706b548bc97d51064ac5479
Chris Martin <ch.martin@gmail.com>
2022-07-09 01:07:35 -0600

    closed type family

--- step/library/step/Step/Action/KindJoin.hs
+++ step/library/step/Step/Action/KindJoin.hs
@@ -13,48 +13,51 @@ type a :> b = KindJoin a b
 -- This function is mostly commutative (@KindJoin a b@ = @KindJoin b a@), with one exception: @KindJoin 'Atom' 'Sure' = 'Atom'@, but @KindJoin 'Sure' 'Atom' = 'Any'@. Atomicity is preserved by composition with a Sure action only if the Atom comes first.
 --
 type family KindJoin (k1 :: ActionKind) (k2 :: ActionKind) :: ActionKind
+  where
 
--- SureQuery is easy: It never changes the kind of whatever it's joined with.
 
-type instance KindJoin SureQuery k = k
-type instance KindJoin k SureQuery = k
+    -- SureQuery is easy: It never changes the kind of whatever it's joined with.
 
--- Some of the action kinds compose very nicely with themselves.
+    KindJoin SureQuery k = k
+    KindJoin k SureQuery = k
 
-type instance KindJoin Any        Any        = Any
-type instance KindJoin Move       Move       = Move
-type instance KindJoin Query      Query      = Query
-type instance KindJoin Sure       Sure       = Sure
+    -- Some of the action kinds compose very nicely with themselves.
 
--- Atomic, however, lose their atomicity when put in sequence with anything fallible.
+    KindJoin Any Any = Any
+    KindJoin Move Move = Move
+    KindJoin Query Query = Query
+    KindJoin Sure Sure = Sure
 
-type instance KindJoin AtomicMove AtomicMove = Move
-type instance KindJoin Atom       Atom       = Any
+    -- Atomic, however, lose their atomicity when put in sequence with anything fallible.
 
--- ...
+    KindJoin AtomicMove AtomicMove = Move
+    KindJoin Atom Atom = Any
 
-type instance KindJoin AtomicMove Move       = Move
-type instance KindJoin Move       AtomicMove = Move
+    -- ...
 
-type instance KindJoin AtomicMove Any        = Move
-type instance KindJoin Any        AtomicMove = Move
+    KindJoin AtomicMove Move = Move
+    KindJoin Move AtomicMove = Move
 
-type instance KindJoin Move       Any        = Move
-type instance KindJoin Any        Move       = Move
+    KindJoin AtomicMove Any = Move
+    KindJoin Any AtomicMove = Move
 
-type instance KindJoin Move       Atom       = Move
-type instance KindJoin Atom       Move       = Move
+    KindJoin Move Any = Move
+    KindJoin Any Move = Move
 
-type instance KindJoin Move       Query      = Move
-type instance KindJoin Query      Move       = Move
+    KindJoin Move Atom = Move
+    KindJoin Atom Move = Move
 
-type instance KindJoin Atom       Any        = Any
-type instance KindJoin Any        Atom       = Any
+    KindJoin Move Query = Move
+    KindJoin Query Move = Move
 
-type instance KindJoin AtomicMove Sure       = AtomicMove
-type instance KindJoin Sure       AtomicMove = Move
+    KindJoin Atom Any = Any
+    KindJoin Any Atom = Any
 
--- This is a weird one! It is the only example where KindJoin is not commutative.
--- Atomicity is preserved by composition with a Sure action only if the Atom comes first.
-type instance KindJoin Atom       Sure       = Atom
-type instance KindJoin Sure       Atom       = Any
+    KindJoin AtomicMove Sure = AtomicMove
+    KindJoin Sure AtomicMove = Move
+
+    -- This is a weird one! It is the only example where KindJoin is not commutative.
+    -- Atomicity is preserved by composition with a Sure action only if the Atom comes first.
+
+    KindJoin Atom Sure = Atom
+    KindJoin Sure Atom = Any
