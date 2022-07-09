43036c7ee915622dc19b845e6162f2cd957315b5
Chris Martin <ch.martin@gmail.com>
2022-07-09 02:22:10 -0600

    finish the (>>) family

--- step/library/step/Step/Action/KindJoin.hs
+++ step/library/step/Step/Action/KindJoin.hs
@@ -6,18 +6,16 @@ import Step.Action.Types
 
 -- | The type @a >> b@ is type of the expression @a >> b@.
 --
--- There is an instance for every pair of action kinds, 49 in total.
---
 -- ==== Non-commutativity
 --
 -- This function is mostly commutative (@(a >> b) = (b >> a)@), with a few exceptions pertaining to atomicity:
 --
--- Atomicity is preserved by composition with a Sure action only if the Atom comes first.
+-- Atomicity is preserved by composition with a Sure action only if the atomic part comes first.
 --
 -- * @'Atom' >> 'Sure' = 'Atom'@, but @'Sure' >> 'Atom' = 'Any'@.
 -- * @'AtomicMove' >> 'Sure' = 'AtomicMove'@, but @'Sure' >> 'AtomicMove' = 'Move'@
 --
--- Atomicity is preserved by composition with a Query only if the Atom comes second.
+-- Atomicity is preserved by composition with a Query only if the atomic part comes second.
 --
 -- * @'Query' >> 'Atom' = 'Atom'@, but @'Atom' >> 'Query' = 'Any'@.
 -- * @'Query' >> 'AtomicMove' = 'AtomicMove'@, but @'AtomicMove' >> 'Query' = 'Move'@.
@@ -25,63 +23,31 @@ import Step.Action.Types
 type family (k1 :: ActionKind) >> (k2 :: ActionKind) :: ActionKind
   where
 
-
-    -- SureQuery is easy: It never changes the kind of whatever it's joined with.
-    -- (15 instances)
-
-    SureQuery >> k = k
-    k >> SureQuery = k
-
-    -- Some of the action kinds compose very nicely with themselves.
-    -- (4 instances)
-
-    Any >> Any = Any
-    Move >> Move = Move
+    -- Query and Sure are closed.
     Query >> Query = Query
     Sure >> Sure = Sure
 
-    -- Atomic, however, lose their atomicity when put in sequence with anything fallible.
-    -- (2 instances)
+    -- SureQuery is subsumed by whatever it is joined with.
+    SureQuery >> k = k
+    k >> SureQuery = k
 
-    AtomicMove >> AtomicMove = Move
-    Atom >> Atom = Any
+    -- Move subsumes anything else it is joined with.
+    k >> Move = Move
+    Move >> k = Move
 
-    -- ...
-
-    AtomicMove >> Move = Move
-    Move >> AtomicMove = Move
-
-    AtomicMove >> Any = Move
-    Any >> AtomicMove = Move
-
-    Move >> Any = Move
-    Any >> Move = Move
-
-    Move >> Atom = Move
-    Atom >> Move = Move
-
-    Move >> Query = Move
-    Query >> Move = Move
-
-    Atom >> Any = Any
-    Any >> Atom = Any
-
-
-    -- Atomicity is preserved by composition with a Sure action only if the Atom comes first.
-    -- (4 instances)
-
-    Atom >> Sure = Atom
-    Sure >> Atom = Any
-
+    -- Atomicity is preserved by join with a Sure action if the atomic step comes first.
+    Atom       >> Sure = Atom
     AtomicMove >> Sure = AtomicMove
-    Sure >> AtomicMove = Move
 
-
-    -- Atomicity is preserved by composition with a Query only if the Atom comes second.
-    -- (4 instances)
-
-    Query >> Atom = Atom
-    Atom >> Query = Any
-
+    -- Atomicity is preserved by join with a Query if the atom step comes second.
+    Query >> Atom       = Atom
     Query >> AtomicMove = AtomicMove
-    AtomicMove >> Query = Move
+
+    -- Atom and AtomicMove lose their atomicity when joined with anything else.
+
+    -- When AtomicMove loses atomicity, it degrades to Move.
+    AtomicMove >> _ = Move
+    _ >> AtomicMove = Move
+
+    -- All other combinations degrade to Any.
+    _ >> _ = Any
