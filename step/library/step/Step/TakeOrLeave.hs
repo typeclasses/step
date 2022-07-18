d6dbac84c2c34ff6552c89ec5e122e15eff99479
Chris Martin <ch.martin@gmail.com>
2022-07-18 01:25:15 -0600

    add leavePrism

--- step/library/step/Step/TakeOrLeave.hs
+++ step/library/step/Step/TakeOrLeave.hs
@@ -1,6 +1,8 @@
 module Step.TakeOrLeave where
 
+import Either (Either (..))
 import Maybe (Maybe (..))
+import Optics (Prism, prism)
 
 data TakeOrLeave b a = Leave b | Take a
 
@@ -13,3 +15,6 @@ fromTake :: TakeOrLeave b a -> Maybe a
 fromTake = \case
     Leave _ -> Nothing
     Take x -> Just x
+
+leavePrism :: Prism (TakeOrLeave b1 a2) (TakeOrLeave b2 a2) b1 b2
+leavePrism = prism Leave \case{ Leave x -> Right x; Take x -> Left (Take x) }
