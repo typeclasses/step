be0df75b1267715eec22c94344141620a15bdcc7
Chris Martin <ch.martin@gmail.com>
2022-07-26 13:01:24 -0600

    BufferSession shorten type var names

--- step/library/step/Step/Buffer/Session.hs
+++ step/library/step/Step/Buffer/Session.hs
@@ -24,45 +24,37 @@ import Step.Input.Stream (Stream (..))
 
 import Step.Buffer.Result (BufferResult(..))
 
-data BufferSession text char = BufferSession
-  { uncommitted :: Buffer text char
-  , unseen :: Buffer text char
+data BufferSession xs x = BufferSession
+  { uncommitted :: Buffer xs x
+  , unseen :: Buffer xs x
   }
 
-newBufferSession :: Buffer text char -> BufferSession text char
+newBufferSession :: Buffer xs x -> BufferSession xs x
 newBufferSession b = BufferSession b b
 
-uncommittedLens :: Lens
-  (BufferSession text char)
-  (BufferSession text char)
-  (Buffer text char)
-  (Buffer text char)
+uncommittedLens :: Lens (BufferSession xs x) (BufferSession xs x) (Buffer xs x) (Buffer xs x)
 uncommittedLens = lens uncommitted \x y -> x{ uncommitted = y }
 
-unseenLens :: Lens
-  (BufferSession text char)
-  (BufferSession text char)
-  (Buffer text char)
-  (Buffer text char)
+unseenLens :: Lens (BufferSession xs x) (BufferSession xs x) (Buffer xs x) (Buffer xs x)
 unseenLens = lens unseen \x y -> x{ unseen = y }
 
-curse :: Monad m => ListLike text char => Session text char (StateT (Buffer text char) m)
+curse :: Monad m => ListLike xs x => Session xs x (StateT (Buffer xs x) m)
 curse = Session{ Session.run, Session.commit, Session.input }
 
-run :: Monad m => ListLike text char => StateT (BufferSession text char) m a -> StateT (Buffer text char) m a
+run :: Monad m => ListLike xs x => StateT (BufferSession xs x) m a -> StateT (Buffer xs x) m a
 run a = do
     b <- get
     (x, bs) <- lift (runStateT a (BufferSession b b))
     put (uncommitted bs)
     return x
 
-input :: Monad m => ListLike text char => Stream (StateT (BufferSession text char) m) (Nontrivial text char)
+input :: Monad m => ListLike xs x => Stream (StateT (BufferSession xs x) m) (Nontrivial xs x)
 input = Stream{ next = zoom unseenLens Buffer.takeChunk }
 
-commit :: Monad m => ListLike text char => Positive Natural -> StateT (BufferSession text char) m AdvanceResult
+commit :: Monad m => ListLike xs x => Positive Natural -> StateT (BufferSession xs x) m AdvanceResult
 commit n = zoom uncommittedLens (Buffer.dropN n)
 
-drink :: Monad m => Stream m (Nontrivial text char) -> StateT (BufferSession text char) m BufferResult
+drink :: Monad m => Stream m (Nontrivial xs x) -> StateT (BufferSession xs x) m BufferResult
 drink xs = lift (Stream.next xs) >>= \case
     Nothing -> return NothingToBuffer
     Just x -> do
