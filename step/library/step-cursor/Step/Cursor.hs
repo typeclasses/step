b7da30f75c759fa5ee5ea44e5b0c1fdfeebdf7d1
Chris Martin <ch.martin@gmail.com>
2022-08-03 00:59:27 -0600

    remove Cursory class

--- step/library/step-cursor/Step/Cursor.hs
+++ step/library/step-cursor/Step/Cursor.hs
@@ -4,7 +4,6 @@ module Step.Cursor
   (
     {- * Cursor -} ReadWriteCursor (..), AdvanceResult (..), pattern Run, inp, com, runn,
         rebaseCursor, lookAhead_,
-    {- * Class -} Cursory (..),
     {- * Stream -}
         Stream (..), stream, streamRST, StreamCompletion (..),
         record,
@@ -15,7 +14,6 @@ module Step.Cursor
 
 import Step.Cursor.AdvanceResult
 import Step.Cursor.Stream
-import Step.Cursor.Class
 import Step.Cursor.StreamCompletion
 import Step.Cursor.ReadWriteCursor
 import Step.Cursor.InputChunking
