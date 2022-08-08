9523d687e3de2e679a8043d6f5bb4867255cd855
Chris Martin <ch.martin@gmail.com>
2022-08-08 17:54:53 -0600

    remove list-transformer again

--- step/library/step-internal/Step/Internal/Prelude.hs
+++ step/library/step-internal/Step/Internal/Prelude.hs
@@ -4,7 +4,6 @@ module Step.Internal.Prelude (module X) where
 
 import Step.Internal.Contravariant as X
 import Step.Internal.Dependencies as X
-import Step.Internal.HoistListT as X
 import Step.Internal.LocOptics as X
 import Step.Internal.Positive as X
 import Step.Internal.Times as X
