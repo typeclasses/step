0187e8b83b5e61411f0aa3eb5b2a8ff43e133ff8
Chris Martin <ch.martin@gmail.com>
2022-08-09 14:45:41 -0600

    haddocks on Is instances doing more clutter than good

--- step/library/step-action-types/Step/ActionTypes/Subtyping.hs
+++ step/library/step-action-types/Step/ActionTypes/Subtyping.hs
@@ -2,12 +2,8 @@
 
 module Step.ActionTypes.Subtyping
   (
-
+    Is (..),
     castTo,
-
-    Is,
-    -- The class's method is not exported because there is no reason to define new instances of the class outside of this module.
-
   )
   where
 
@@ -19,45 +15,29 @@ import Step.ActionTypes.Fallible
 
 import Coerce (coerce)
 
-
 class Is (act1 :: Action) (act2 :: Action) where
     cast :: Monad m => act1 xs x r s e m a -> act2 xs x r s e m a
 
-
-castTo :: forall act2 act1 xs x r s e m a. (Monad m, Is act1 act2) => act1 xs x r s e m a -> act2 xs x r s e m a
+castTo :: forall act2 act1 xs x r s e m a. (Monad m, Is act1 act2) =>
+    act1 xs x r s e m a -> act2 xs x r s e m a
 castTo = cast @act1 @act2
 
-
--- Identity
-
--- | Everything trivially lifts to itself
+-- Everything trivially lifts to itself
 instance Is Any Any where cast = id
-
--- | Everything trivially lifts to itself
 instance Is Query Query where cast = id
-
--- | Everything trivially lifts to itself
 instance Is Move Move where cast = id
-
--- | Everything trivially lifts to itself
 instance Is Atom Atom where cast = id
-
--- | Everything trivially lifts to itself
 instance Is AtomicMove AtomicMove where cast = id
-
--- | Everything trivially lifts to itself
 instance Is Sure Sure where cast = id
-
--- | Everything trivially lifts to itself
 instance Is SureQuery SureQuery where cast = id
-
--- | Everything trivially lifts to itself
 instance Is Fail Fail where cast = id
 
-
--- Any supertypes everything else
-
--- | Everything lifts to Any
+-- Everything lifts to Any
+instance Is Move Any where cast = coerce
+instance Is AtomicMove Any where cast = cast @Atom @Any . coerce
+instance Is Sure Any where cast (Sure x) = mapError absurd x
+instance Is SureQuery Any where cast (SureQuery x) = castTo @Any (mapError absurd x)
+instance Is Atom Any where cast (Atom q) = Any_Join (castTo @Any (fmap (castTo @Any) q))
 instance Is Query Any where
     cast = \case
         Query_Lift x -> Any_Lift x
@@ -68,80 +48,22 @@ instance Is Query Any where
         Query_Fail f -> Any_Fail f
         Query_Reset x -> Any_Reset x
 
--- | Everything lifts to Any
-instance Is Move Any where cast = coerce
-
--- | Everything lifts to Any
-instance Is AtomicMove Any where cast = cast @Atom @Any . coerce
-
--- | Everything lifts to Any
-instance Is Sure Any where
-    cast :: forall xs x r s e m a. Monad m => Sure xs x r s e m a -> Any xs x r s e m a
-    cast (Sure x) = mapError absurd x
-
--- | Everything lifts to Any
-instance Is SureQuery Any where
-    cast (SureQuery x) = castTo @Any (mapError absurd x)
-
--- | Everything lifts to Any
-instance Is Atom Any where
-    cast (Atom q) = Any_Join (castTo @Any (fmap (castTo @Any) q))
-
-
 -- Atom + Move = AtomicMove
-
--- | AtomicMove gets its name from the fact that it has the properties of both Atom and Move
-instance Is AtomicMove Move where
-    cast = coerce . cast @Atom @Any . coerce
-
--- | AtomicMove gets its name from the fact that it has the properties of both Atom and Move
-instance Is AtomicMove Atom where
-    cast = coerce
-
+instance Is AtomicMove Move where cast = coerce . cast @Atom @Any . coerce
+instance Is AtomicMove Atom where cast = coerce
 
 -- Sure + Query = SureQuery
-
--- | SureQuery gets its name from the fact that it has the properties of both Sure and Query
-instance Is SureQuery Sure where
-  cast :: forall xs x r s e m a. Monad m => SureQuery xs x r s e m a -> Sure xs x r s e m a
-  cast (SureQuery q) = Sure (cast q)
-
--- | SureQuery gets its name from the fact that it has the properties of both Sure and Query
-instance Is SureQuery Query where
-  cast :: forall xs x r s e m a. Monad m => SureQuery xs x r s e m a -> Query xs x r s e m a
-  cast (SureQuery q) = mapError absurd q
-
+instance Is SureQuery Sure where cast (SureQuery q) = Sure (cast q)
+instance Is SureQuery Query where cast (SureQuery q) = mapError absurd q
 
 -- Trivial subtypes of Atom
+instance Is Query Atom where cast = Atom . fmap return
+instance Is Sure Atom where cast = Atom . return
+instance Is SureQuery Atom where cast (SureQuery q) = Atom $ fmap return $ mapError absurd q
 
--- | A Query is trivially atomic because it never moves the cursor, therefore it cannot move and fail
-instance Is Query Atom where
-    cast = Atom . fmap return
-
--- | A Sure action is trivially atomic because it never fails, therefore it cannot move and fail
-instance Is Sure Atom where
-    cast = Atom . return
-
-instance Is SureQuery Atom where
-    cast (SureQuery q) = Atom $ fmap return $ mapError absurd q
-
-
--- | Fail casts to anything that isn't Sure
-instance Is Fail Any where
-    cast (Fail f) = Any_Fail f
-
--- | Fail casts to anything that isn't Sure
-instance Is Fail Query where
-    cast (Fail f) = Query_Fail f
-
--- | Fail casts to anything that isn't Sure
-instance Is Fail Move where
-    cast (Fail f) = Move (Any_Fail f)
-
--- | Fail casts to anything that isn't Sure
-instance Is Fail Atom where
-    cast (Fail f) = Atom (Query_Fail f)
-
--- | Fail casts to anything that isn't Sure
-instance Is Fail AtomicMove where
-    cast (Fail f) = AtomicMove (Atom (Query_Fail f))
+-- Fail casts to anything that isn't Sure
+instance Is Fail Any where cast (Fail f) = Any_Fail f
+instance Is Fail Query where cast (Fail f) = Query_Fail f
+instance Is Fail Move where cast (Fail f) = Move (Any_Fail f)
+instance Is Fail Atom where cast (Fail f) = Atom (Query_Fail f)
+instance Is Fail AtomicMove where cast (Fail f) = AtomicMove (Atom (Query_Fail f))
