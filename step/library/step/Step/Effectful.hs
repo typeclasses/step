ac6f481381cdbd0cd04efc05b85495b6729a541e
Chris Martin <ch.martin@gmail.com>
2022-08-22 15:13:25 -0600

    still problems with casting
    https://github.com/haskell-effectful/effectful/discussions/91

--- step/library/step/Step/Effectful.hs
+++ step/library/step/Step/Effectful.hs
@@ -106,19 +106,29 @@ data Mode = ReadOnly | ReadWrite
 
 type instance DispatchOf (Step mo xs x) = 'Dynamic
 
-runStep :: forall xs x es a. '[Load xs x, Buffer 'CommitBuffer xs x] :>> es => NT.DropOperation xs x
+runStepR :: forall xs x es a. '[Load xs x, Buffer 'CommitBuffer xs x] :>> es =>
+    Eff (Step 'ReadOnly xs x ': es) a -> Eff es a
+runStepR = reinterpret runStepHandler stepHandlerR
+
+stepHandlerR :: forall xs x es. '[Load xs x, Buffer 'CommitBuffer xs x] :>> es =>
+    EffectHandler (Step 'ReadOnly xs x) (Buffer 'ViewBuffer xs x ': es)
+stepHandlerR _env = \case
+    Reset -> getBufferSeq @'CommitBuffer @xs @x >>= putBufferSeq @'ViewBuffer @xs @x
+    Next -> runStepNext
+
+runStepRW :: forall xs x es a. '[Load xs x, Buffer 'CommitBuffer xs x] :>> es => NT.DropOperation xs x
     -> Eff (Step 'ReadWrite xs x ': es) a -> Eff es a
-runStep dropOp = reinterpret runStepHandler (stepHandler dropOp)
+runStepRW dropOp = reinterpret runStepHandler (stepHandlerRW dropOp)
 
-runStepHandler :: forall xs x es a. Buffer 'CommitBuffer xs x :> es => Eff (Buffer 'ViewBuffer xs x ': es) a -> Eff es a
-runStepHandler a = getBufferSeq @'CommitBuffer @xs @x >>= \b -> runBuffer' @'ViewBuffer @xs @x b a
-
-stepHandler :: forall xs x es. '[Load xs x, Buffer 'CommitBuffer xs x] :>> es => NT.DropOperation xs x
+stepHandlerRW :: forall xs x es. '[Load xs x, Buffer 'CommitBuffer xs x] :>> es => NT.DropOperation xs x
     -> EffectHandler (Step 'ReadWrite xs x) (Buffer 'ViewBuffer xs x ': es)
-stepHandler dropOp _env = \case
+stepHandlerRW dropOp _env = \case
     Reset -> getBufferSeq @'CommitBuffer @xs @x >>= putBufferSeq @'ViewBuffer @xs @x
     Next -> runStepNext
     Commit n -> runStepCommit @xs @x dropOp n
+
+runStepHandler :: forall xs x es a. Buffer 'CommitBuffer xs x :> es => Eff (Buffer 'ViewBuffer xs x ': es) a -> Eff es a
+runStepHandler a = getBufferSeq @'CommitBuffer @xs @x >>= \b -> runBuffer' @'ViewBuffer @xs @x b a
 
 runStepNext :: forall xs x es. '[Load xs x, Buffer 'ViewBuffer xs x, Buffer 'CommitBuffer xs x] :>> es => Eff es (Maybe (Nontrivial xs x))
 runStepNext = takeBufferChunk @'ViewBuffer >>= \case
@@ -135,54 +145,117 @@ bufferMore = load @xs @x >>= \case
     Nothing -> return False
     Just x -> do{ feedBuffer @'CommitBuffer x; feedBuffer @'ViewBuffer x; return True }
 
-castStep :: Step 'ReadOnly xs x m1 a -> Step 'ReadWrite xs x m2 a
-castStep = \case{ Next -> Next; Reset -> Reset }
+-- ⭕ The Walk kind
 
-castStepEffect :: forall xs x es a. Step 'ReadWrite xs x :> es => Eff (Step 'ReadOnly xs x ': es) a -> Eff es a
-castStepEffect = interpret \_ -> send . castStep
+-- | A "walk" is a collection of "steps"
+type Walk = Type -> Type -> [Effect] -> Type -> Type -> Type
 
--- ⭕ Simple actions that are just a newtype for an action with a Step effect
+-- ⭕ Simple walks that are just a newtype for an Eff with a Step effect
 
--- | The most general of the actions
-newtype Any xs x es e a = Any (Eff (Step 'ReadWrite xs x ': Error e ': es) a)
-    deriving newtype (Functor, Applicative, Monad)
+type Any :: Walk
+type Query :: Walk
+type Sure :: Walk
+type SureQuery :: Walk
+
+-- | The most general of the walks
+data Any xs x es e a = forall es'.
+    Subset (Step 'ReadWrite xs x ': Step 'ReadOnly xs x ': Error e ': es) es' =>
+    Any (Eff es' a)
+    -- deriving newtype (Functor, Applicative, Monad)
 
 -- | Like 'Any', but cannot move the cursor
-newtype Query xs x es e a = Query (Eff (Step 'ReadOnly xs x ': Error e ': es) a)
-    deriving newtype (Functor, Applicative, Monad)
+data Query xs x es e a = forall es'.
+    Subset (Step 'ReadOnly xs x ': Error e ': es) es' =>
+    Query (Eff es' a)
+    -- deriving newtype (Functor, Applicative, Monad)
 
 -- | Always succeeds
-newtype Sure xs x es e a = Sure (Eff (Step 'ReadWrite xs x ': es) a)
-    deriving newtype (Functor, Applicative, Monad)
+data Sure xs x es e a =
+    forall es'.
+    ( Step 'ReadWrite xs x :> es'
+    , Step 'ReadOnly xs x :> es'
+    , es :>> es'
+    ) =>
+    Sure (Eff es' a)
+    -- deriving newtype (Functor, Applicative, Monad)
 
 -- | Always succeeds, does not move the cursor
-newtype SureQuery xs x es e a = SureQuery (Eff (Step 'ReadOnly xs x ': es) a)
-    deriving newtype (Functor, Applicative, Monad)
+data SureQuery xs x es e a =
+    forall es'.
+    ( Step 'ReadOnly xs x :> es'
+    , es :>> es'
+    ) =>
+    SureQuery (Eff es' a)
+    -- deriving newtype (Functor, Applicative, Monad)
 
--- ⭕ Actions defines in terms of others
+-- ⭕ Walks defined in terms of others
+
+type Atom :: Walk
+type Move :: Walk
+type AtomicMove :: Walk
 
 -- | Fails noncommittally; see 'try'
-newtype Atom xs x es e a = Atom{ unAtom :: Query xs x es e (Sure xs x es e a) }
-    deriving stock (Functor)
+newtype Atom xs x es e a = Atom (Query xs x es e (Sure xs x es e a))
+    -- deriving stock (Functor)
 
 -- | Always moves the cursor
 newtype Move xs x es e a = Move (Any xs x es e a)
-    deriving stock (Functor)
+    -- deriving stock (Functor)
 
 -- | Always moves the cursor, is atomic
-newtype AtomicMove xs x es e a = AtomicMove{ unAtomicMove :: Atom xs x es e a }
+newtype AtomicMove xs x es e a = AtomicMove (Atom xs x es e a)
+    -- deriving stock (Functor)
+
+-- instance (TypeError ('Text "Atom cannot be Applicative because (<*>) would not preserve atomicity")) => Applicative (Atom xs x e m) where
+--     pure = error "unreachable"
+--     (<*>) = error "unreachable"
+
+-- instance (TypeError ('Text "Move cannot be Applicative because 'pure' would not move the cursor")) =>
+--     Applicative (Move xs x e es)
+--   where
+--     pure = error "unreachable"
+--     (<*>) = error "unreachable"
+
+-- instance (TypeError ('Text "AtomicMove cannot be Applicative because 'pure' would not move the cursor and (<*>) would not preserve atomicity")) => Applicative (AtomicMove xs x e m) where
+--     pure = error "unreachable"
+--     (<*>) = error "unreachable"
+
+-- ⭕ Failure
+
+type Fail :: Walk
+
+newtype Fail xs x es e a = Fail (Eff (Error e ': es) Void)
     deriving stock (Functor)
 
-instance (TypeError ('Text "Atom cannot be Applicative because (<*>) would not preserve atomicity")) => Applicative (Atom xs x e m) where
-    pure = error "unreachable"
-    (<*>) = error "unreachable"
+-- ⭕ Class for action subtype relationship
 
-instance (TypeError ('Text "Move cannot be Applicative because 'pure' would not move the cursor")) =>
-    Applicative (Move xs x e es)
-  where
-    pure = error "unreachable"
-    (<*>) = error "unreachable"
+class Is (act1 :: Walk) (act2 :: Walk) where
+    cast :: act1 xs x es e a -> act2 xs x es e a
 
-instance (TypeError ('Text "AtomicMove cannot be Applicative because 'pure' would not move the cursor and (<*>) would not preserve atomicity")) => Applicative (AtomicMove xs x e m) where
-    pure = error "unreachable"
-    (<*>) = error "unreachable"
+castTo :: forall act2 act1 xs x es e a. Is act1 act2 => act1 xs x es e a -> act2 xs x es e a
+castTo = cast @act1 @act2
+
+-- ⭕ Everything is itself
+
+instance {-# overlappable #-} Is a a where
+    cast = id
+
+-- ⭕ Casting out of read-only
+
+castRW :: forall xs x es a. Step 'ReadOnly xs x :> es => Eff es a -> Eff (Step 'ReadWrite xs x ': es) a
+castRW = raise
+
+instance Is SureQuery Sure where
+    -- cast :: forall xs x es e a. SureQuery xs x es e a -> Sure xs x es e a
+    -- cast (SureQuery (x :: Eff es' a)) = Sure (inject x :: Eff (Step 'ReadWrite xs x ': es') a)
+
+instance Is Query Any where
+    -- cast (Query x) = Any (raise x)
+
+-- ⭕ Casting out of sureness
+
+instance Is SureQuery Query where
+    -- cast (SureQuery x) = Query (raise x)
+
+instance Is Sure Any where
+    -- cast (Sure x) = Any (raise x)
