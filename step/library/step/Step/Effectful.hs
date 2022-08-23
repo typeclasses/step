bb740d1fa14152fdd47cac393fad1b84aad26d9a
Chris Martin <ch.martin@gmail.com>
2022-08-23 03:31:38 -0600

    copy over the >> type family

--- step/library/step/Step/Effectful.hs
+++ step/library/step/Step/Effectful.hs
@@ -342,3 +342,53 @@ instance Fallible AtomicMove where
 
 instance Fallible a => Is Failure a where
     cast (Failure x) = fail x
+
+-- ⭕
+
+-- | The type @a >> b@ is type of the expression @a >> b@.
+--
+-- This function is mostly not commutative (@a >> b@ is not the same as @b >> a@) because whether an atomic action'atomicity is preserved depends on the order of the composition in some cases.
+
+type family (act1 :: Action) >> (act2 :: Action) :: Action
+  where
+
+    -- When failure is first, the second step is irrelevant.
+    Failure >> k = Failure
+
+    -- When failure is second, sureness and atomicity are lost.
+    Sure >> Failure = Any
+    SureQuery >> Failure = Query
+    Atom >> Failure = Any
+    AtomicMove >> Failure = Move
+    k >> Failure = k
+
+    -- Joining with SureQuery has no effect on the type
+    SureQuery >> k = k
+    k >> SureQuery = k
+
+    -- Properties other than atomicity are closed under composition.
+    Query >> Query = Query
+    Sure >> Sure = Sure
+    -- (Move >> Move = Move) is covered later below.
+
+    -- When an atomic step is followed by an infallible step, atomicity is preserved.
+    Atom >> Sure = Atom
+    AtomicMove >> Sure = AtomicMove
+    -- (>> SureQuery) has already been covered above.
+
+    -- When an atomic step is preceded by a query, atomicity is preserved.
+    Query >> Atom = Atom
+    Query >> Sure = Atom
+    Query >> AtomicMove = AtomicMove
+    -- (SureQuery >>) has already been covered above.
+
+    -- Movement of a part implies movement of the whole.
+    k >> Move = Move
+    Move >> k = Move
+
+    -- When AtomicMove loses atomicity, it degrades to Move.
+    AtomicMove >> _ = Move
+    _ >> AtomicMove = Move
+
+    -- All other combinations degrade to Any.
+    _ >> _ = Any
