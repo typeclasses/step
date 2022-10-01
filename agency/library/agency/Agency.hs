b451cda68a6e1685741cd6286a90aaad13fead2b
Chris Martin <ch.martin@gmail.com>
2022-09-30 23:38:23 -0600

    separate types, connectDaemonToClient

--- agency/library/agency/Agency.hs
+++ agency/library/agency/Agency.hs
@@ -2,75 +2,71 @@ module Agency where
 
 import Relude hiding (Proxy)
 
-type Response = Type -> Type
+data Client (up :: Type -> Type) (m :: Type -> Type) (a :: Type) where
+    Pure :: a -> Client up m a
+    Action :: m a -> Client up m a
+    Request :: up a -> Client up m a
+    Bind :: Client up m x -> (x -> Client up m a) -> Client up m a
 
-type Context = Type -> Type
+newtype Handler up down m =
+    Handler (forall a. down a -> Client up m (Daemon up down m, a))
 
-data Agent (am :: Maybe Response) (bm :: Maybe Response) (m :: Context) r where
+newtype Daemon (up :: Type -> Type) (down :: Type -> Type) (m :: Type -> Type) =
+    Daemon (Client up m (Handler up down m))
 
-    -- | 'pure'
-    AgentPure :: r -> Agent am bm m r
-
-    -- | ('>>=')
-    AgentBind :: Agent am bm m x -> (x -> Agent am bm m r) -> Agent am bm m r
-
-    -- | Action lifted from the base monad @m@
-    AgentAction :: m r -> Agent am bm m r
-
-    -- | Request of type @a x@ + continuation from response type @x@
-    AgentRequest :: a r -> Agent ('Just a) bm m r
-
-    -- | Continuation from request type @b x@ producing a response of type @x@
-    AgentServe :: (forall x. b x -> Agent am 'Nothing m (r, x)) -> Agent am ('Just b) m r
-
-type Daemon am b m = Agent am ('Just b) m Void
-
-instance Functor m => Functor (Agent am bm m) where
+instance Functor m => Functor (Client up m) where
     fmap f = \case
-        AgentAction x -> AgentAction (fmap f x)
-        a -> AgentBind a (AgentPure . f)
+        Pure x -> Pure (f x)
+        Action x -> Action (fmap f x)
+        Request x -> Bind (Request x) (Pure . f)
+        Bind x g -> Bind x (fmap (fmap f) g)
 
-instance Functor m => Applicative (Agent am bm m) where
-    pure = AgentPure
-    a1 <*> a2 = AgentBind a1 \f -> a2 <&> \x -> f x
-    a1 *> a2 = AgentBind a1 \_ -> a2
+instance Functor m => Applicative (Client up m) where
+    pure = Pure
+    a1 <*> a2 = Bind a1 \f -> a2 <&> \x -> f x
+    a1 *> a2 = Bind a1 \_ -> a2
 
-instance Functor m => Monad (Agent am bm m) where
-    (>>=) = AgentBind
+instance Functor m => Monad (Client up m) where
+    (>>=) = Bind
 
-runAgent :: Monad m => Agent 'Nothing 'Nothing m r -> m r
-runAgent = \case
-    AgentPure x -> pure x
-    AgentAction x -> x
-    AgentBind x f -> runAgent x >>= (runAgent . f)
+data Nil a
 
-type Connection am b cm m r = Agent am cm m (Daemon am b m, r)
+run :: Monad m => Client Nil m a -> m a
+run = \case
+    Pure x -> pure x
+    Action x -> x
+    Bind x f -> run x >>= (run . f)
 
-connect :: Functor m => Daemon am b m -> Agent ('Just b) cm m r -> Connection am b cm m r
+connectDaemonToClient :: Functor m => Daemon up x m -> Client x m a -> Client up m (Daemon up x m, a)
+connectDaemonToClient up down =
+    case down of
+        Pure x -> Pure (up, x)
+        Action x -> Action (fmap (up,) x)
+        Bind x f -> connectDaemonToClient up x `Bind` \(up', y) -> connectDaemonToClient up' (f y)
+        Request r ->
+            case up of
+                Daemon (Pure (Handler h)) -> h r
+                Daemon (Action x) -> Action x `Bind` \(Handler h) -> h r
+                Daemon (Request r') -> Request r' `Bind` \(Handler h) -> h r
+                Daemon (Bind x f) -> x `Bind` \y -> f y `Bind` \(Handler h) -> h r
 
-connect up (AgentPure x) = fmap (up,) (AgentPure x)
-connect up (AgentAction x) = AgentAction (x <&> (up,))
-connect up (AgentBind y g) = connect up y >>= \(up', z) -> connect up' (g z)
-connect up (AgentServe f) = AgentServe \x -> connect up (f x) <&> \(up', (r, y)) -> ((up', r), y)
+-- connectDaemonToDaemon :: Functor m => Daemon up x m -> Daemon x down m -> Daemon up down m
+-- connectDaemonToDaemon up down =
+--     case down of
+--         Daemon (Pure (Handler h)) -> Daemon (Pure (Handler _))
 
-connect (AgentBind (AgentServe f) g) (AgentRequest x) = relaxAgentDown (f x) <&> \(r, y) -> (g r, y)
+-- (>->) :: Functor m => Daemon am b m -> Agent ('Just b) cm m r -> Agent am cm m r
+-- a >-> b = fmap snd (connect a b)
 
--- These cases involving 'absurd' don't really happen, but they satisfy the type checker
-connect (AgentAction x) (AgentRequest _) = AgentAction (fmap absurd x)
-connect (AgentServe f) (AgentRequest x) = relaxAgentDown (f x) >>= \(r, _) -> absurd r
-
-(>->) :: Functor m => Daemon am b m -> Agent ('Just b) cm m r -> Agent am cm m r
-a >-> b = fmap snd (connect a b)
-
-relaxAgentDown :: Functor m => Agent am 'Nothing m r -> Agent am cm m r
-relaxAgentDown = r
-  where
-    r :: Functor m => Agent am 'Nothing m r -> Agent am cm m r
-    r = \case
-      AgentRequest x -> AgentRequest x
-      AgentAction x -> AgentAction x
-      AgentBind x f -> AgentBind (r x) (fmap r f)
-      AgentPure x -> AgentPure x
+-- relaxAgentDown :: Functor m => Agent am 'Nothing m r -> Agent am cm m r
+-- relaxAgentDown = r
+--   where
+--     r :: Functor m => Agent am 'Nothing m r -> Agent am cm m r
+--     r = \case
+--       AgentRequest x -> AgentRequest x
+--       AgentAction x -> AgentAction x
+--       AgentBind x f -> AgentBind (r x) (fmap r f)
+--       AgentPure x -> AgentPure x
 
 {-
 
