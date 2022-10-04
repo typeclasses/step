91a79331c48e727faeddc796821bd3bfbf15cc7c
Chris Martin <ch.martin@gmail.com>
2022-10-04 00:47:28 -0600

    futz

--- supply-chain/library/supply-chain/SupplyChain/Core.hs
+++ supply-chain/library/supply-chain/SupplyChain/Core.hs
@@ -1,14 +1,16 @@
 module SupplyChain.Core where
 
 import Control.Applicative (Applicative (pure, (*>), (<*>)), (<$>))
-import Control.Monad (Monad((>>=)), Functor (fmap))
+import Control.Monad (Monad ((>>=)), Functor (fmap))
 import Data.Function (($), (.))
 import Data.Functor (Functor (fmap), (<$>), (<&>))
 import Data.Kind (Type)
 
+
 type Action = Type -> Type
 
 type Interface = Type -> Type
+
 
 data Client (up :: Interface) (action :: Action) (product :: Type)
   where
@@ -30,16 +32,17 @@ instance Functor action => Functor (Client up action)
           Request request   ->  Bind (Request request) (Pure . f)
           Bind step1 step2  ->  Bind step1 (client_fmap_f . step2)
 
-instance Functor m => Applicative (Client a m)
+instance Functor action => Applicative (Client up action)
   where
     pure = Pure
 
     a1 <*> a2 = a1 `Bind` \f -> (f $) <$> a2
     a1  *> a2 = a1 `Bind` \_ ->           a2
 
-instance Functor m => Monad (Client a m)
+instance Functor action => Monad (Client up action)
   where
     (>>=) = Bind
+
 
 newtype Vendor (up :: Interface) (down :: Interface) (action :: Action) =
   Vendor
@@ -50,6 +53,7 @@ newtype Vendor (up :: Interface) (down :: Interface) (action :: Action) =
           )
     }
 
+
 data Supply (up :: Interface) (down :: Interface) (action :: Action) (product :: Type) =
   Supply
     { supplyNext :: Vendor up down action
@@ -58,32 +62,42 @@ data Supply (up :: Interface) (down :: Interface) (action :: Action) (product ::
 
 deriving stock instance Functor (Supply up down action)
 
-connectVendorToClient :: Functor m => Vendor a b m -> Client b m r -> Client a m (Supply a b m r)
-connectVendorToClient up =
-  \case
-    Pure    x    ->  Pure                    Supply{ supplyNext = up, supplyProduct = x }
-    Perform act  ->  Perform $ act <&> \x -> Supply{ supplyNext = up, supplyProduct = x }
-    Bind    x f  ->  connectVendorToClient up x `Bind`
-                       \Supply{ supplyNext = up', supplyProduct = y } ->
-                          connectVendorToClient up' (f y)
-    Request r    ->  connectVendorToRequest up r
 
-connectVendorToRequest :: Vendor a b m -> b r -> Client a m (Supply a b m r)
+connectVendorToClient :: forall up down action product. Functor action =>
+    Vendor up down action -> Client down action product -> Client up action (Supply up down action product)
+
+connectVendorToClient vendor =
+  \case
+    Pure product      ->  Pure Supply{ supplyNext = vendor, supplyProduct = product }
+    Perform action    ->  Perform (action <&> \product -> Supply{ supplyNext = vendor, supplyProduct = product })
+    Request request   ->  connectVendorToRequest vendor request
+    Bind step1 step2  ->  connectVendorToClient vendor step1 `Bind` \supply ->
+                            connectVendorToClient (supplyNext supply) (step2 (supplyProduct supply))
+
+connectVendorToRequest :: forall up down action product.
+    Vendor up down action -> down product -> Client up action (Supply up down action product)
+
 connectVendorToRequest up =
   case runVendor up of
-    Pure h      ->  h
-    Perform x   ->  \r -> Perform x          `Bind` \h -> h r
-    Request r'  ->  \r -> Request r'         `Bind` \h -> h r
-    Bind x f    ->  \r -> x `Bind` \y -> f y `Bind` \h -> h r
+    Pure handle       ->  \request ->                                              handle request
+    Perform action    ->  \request -> Perform action             `Bind` \handle -> handle request
+    Request request'  ->  \request -> Request request'           `Bind` \handle -> handle request
+    Bind step1 step2  ->  \request -> step1 `Bind` \x -> step2 x `Bind` \handle -> handle request
 
-connectVendorToVendor :: Functor m => Vendor a b m -> Vendor b c m -> Vendor a c m
+
+connectVendorToVendor :: forall up middle down action. Functor action =>
+    Vendor up middle action -> Vendor middle down action -> Vendor up down action
+
 connectVendorToVendor up (Vendor down) =
   Vendor $
-    connectVendorToClient up down <&> \s r ->
-      connectVendorToClient (supplyNext s) (supplyProduct s r) <&>
+    connectVendorToClient up down <&> \supply request ->
+      connectVendorToClient (supplyNext supply) (supplyProduct supply request) <&>
         supplyJoin
 
-supplyJoin :: Functor action => Supply a b action (Supply b c action product) -> Supply a c action product
+
+supplyJoin :: forall up middle down action product. Functor action =>
+    Supply up middle action (Supply middle down action product) -> Supply up down action product
+
 supplyJoin s =
   Supply
     { supplyNext = connectVendorToVendor (supplyNext s) (supplyNext (supplyProduct s))
