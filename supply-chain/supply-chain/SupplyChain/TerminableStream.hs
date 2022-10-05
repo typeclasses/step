2d98a1f8a4106acccba541580b3d8d42f88abfc9
Chris Martin <ch.martin@gmail.com>
2022-10-05 17:40:19 -0600

    unfinished start of ListT

--- supply-chain/supply-chain/SupplyChain/TerminableStream.hs
+++ supply-chain/supply-chain/SupplyChain/TerminableStream.hs
@@ -1,7 +1,8 @@
 module SupplyChain.TerminableStream
   (
     {- * Interface -} TerminableStream (..),
-    {- * Vendors -} list, nil, while, concatMap, concat,
+    {- * Vendors -} list, nil, map, concatMap, concat, while,
+    {- * Vendor composition -} append, concatMapVendor,
   )
   where
 
@@ -19,6 +20,9 @@ import Data.Maybe (Maybe (..))
 data TerminableStream item response =
     (response ~ Maybe item) => NextMaybe
         -- ^ The next item, or 'Nothing' if input is exhausted
+        --
+        -- It is assumed that after a 'Nothing' response is given,
+        -- all subsequent responses will also be 'Nothing'.
 
 type TerminableStream :: Type -> Interface
 
@@ -47,18 +51,13 @@ nil = go
     go = Vendor \NextMaybe -> pure $ Nothing :-> go
 
 
--- | Yields the longest stream prefix matching the predicate
+-- | Apply a function to each item in the stream
 
-while :: forall a action. Functor action =>
-    (a -> Bool)
-    -> Vendor (TerminableStream a) (TerminableStream a) action
-
-while ok = v
-  where
-    v = Vendor \NextMaybe ->
-        order NextMaybe >>= \case
-            Just x | ok x  ->  pure $ Just x  :-> v
-            _              ->  pure $ Nothing :-> nil
+map :: forall a b action. Functor action =>
+    (a -> b) -> Vendor (TerminableStream a) (TerminableStream b) action
+map f = Vendor \NextMaybe -> order NextMaybe >>= \case
+    Nothing -> pure $ Nothing :-> nil
+    Just a -> pure $ Just (f a) :-> map f
 
 
 {-| Applies the function to each result obtained from upstream,
@@ -78,9 +77,48 @@ concatMap f = go []
             Just a -> offer (go (f a)) NextMaybe
 
 
+concatMapVendor :: forall up a b action. Functor action =>
+    (a -> Vendor up (TerminableStream b) action)
+    -> Vendor (TerminableStream a) (TerminableStream b) action
+
+concatMapVendor f = Vendor \NextMaybe -> order NextMaybe >>= \case
+    Nothing -> pure $ Nothing :-> nil
+    Just x -> offer (append (f x) _) NextMaybe
+    --     Just x :-> v1' -> _ (f x <> (ListT v1' >>= f))
+
+
 -- | Flattens a stream of lists
 
 concat :: forall a action. Functor action =>
     Vendor (TerminableStream [a]) (TerminableStream a) action
 
 concat = concatMap id
+
+
+-- | Yields the longest stream prefix matching the predicate
+
+while :: forall a action. Functor action =>
+    (a -> Bool)
+    -> Vendor (TerminableStream a) (TerminableStream a) action
+
+while ok = v
+  where
+    v = Vendor \NextMaybe ->
+        order NextMaybe >>= \case
+            Just x | ok x  ->  pure $ Just x  :-> v
+            _              ->  pure $ Nothing :-> nil
+
+
+{-| Yields all the items of the first stream,
+    followed by all the items of the second
+-}
+
+append :: forall up a action. Functor action =>
+       Vendor up (TerminableStream a) action
+    -> Vendor up (TerminableStream a) action
+    -> Vendor up (TerminableStream a) action
+
+append a b = Vendor \NextMaybe ->
+    offer a NextMaybe >>= \case
+        Nothing :-> _ -> offer b NextMaybe
+        Just x :-> a' -> pure $ Just x :-> append a' b
