6bc69220ace0cbe9a3cb9786fa1f30c5ebd4e5fa
Chris Martin <ch.martin@gmail.com>
2022-10-05 12:43:21 -0600

    refigure tests

--- supply-chain/test-supply-chain/SupplyChain/Spec.hs
+++ supply-chain/test-supply-chain/SupplyChain/Spec.hs
@@ -5,34 +5,38 @@ import Prelude
 import Control.Monad (replicateM)
 
 import Test.Tasty
-import Test.Tasty.HUnit ((@?=), testCase)
+import Test.Tasty.HUnit ((@?=), testCase, Assertion)
 
 import SupplyChain.Base
 import qualified SupplyChain.More as SC
 
 tests :: TestTree
 tests = testGroup "SupplyChain tests"
-  [ testCase "list" $
-      let
-        x =
-            SC.list "abc"
-            >-> replicateM 4 (order SC.NextMaybe)
-      in
-        SC.eval SC.nil x @?= [Just 'a', Just 'b', Just 'c', Nothing]
-  , testCase "finiteConcat with more" $
-      let
-        x =
-            SC.list ["a", "bc", "def", "ghij"]
-            >-> SC.finiteConcat
-            >-> replicateM 5 (order SC.NextMaybe)
-      in
-        SC.eval SC.nil x @?= [Just 'a', Just 'b', Just 'c', Just 'd', Just 'e']
-  , testCase "finiteConcat exhaustion" $
-      let
-        x =
-            SC.list ["a", "bc"]
-            >-> SC.finiteConcat
-            >-> replicateM 5 (order SC.NextMaybe)
-      in
-        SC.eval SC.nil x @?= [Just 'a', Just 'b', Just 'c', Nothing, Nothing]
-  ]
+    [ testCase "list" list
+    , testGroup "finiteConcat"
+        [ testCase "with more" finiteConcat1
+        , testCase "exhaustion" finiteConcat2
+        ]
+    ]
+
+list :: Assertion
+list = SC.eval SC.nil supplyChain @?= result
+  where
+    supplyChain = SC.list "abc" >-> replicateM 4 (order SC.NextMaybe)
+    result = [Just 'a', Just 'b', Just 'c', Nothing]
+
+finiteConcat1 :: Assertion
+finiteConcat1 =
+    SC.eval SC.nil supplyChain @?= result
+  where
+    supplyChain = SC.list ["a", "bc", "def", "ghij"] >-> SC.finiteConcat
+                    >-> replicateM 5 (order SC.NextMaybe)
+    result = [Just 'a', Just 'b', Just 'c', Just 'd', Just 'e']
+
+finiteConcat2 :: Assertion
+finiteConcat2 =
+    SC.eval SC.nil supplyChain @?= result
+  where
+    supplyChain = SC.list ["a", "bc"] >-> SC.finiteConcat
+                    >-> replicateM 5 (order SC.NextMaybe)
+    result = [Just 'a', Just 'b', Just 'c', Nothing, Nothing]
