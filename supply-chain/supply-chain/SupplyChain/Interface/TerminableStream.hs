7e1ceb5d801d1ff663718af726b88a2d15ca6bc5
Chris Martin <ch.martin@gmail.com>
2022-10-06 10:07:30 -0600

    'group'

--- supply-chain/supply-chain/SupplyChain/Interface/TerminableStream.hs
+++ supply-chain/supply-chain/SupplyChain/Interface/TerminableStream.hs
@@ -1,7 +1,7 @@
 module SupplyChain.Interface.TerminableStream
   (
     {- * Interface -} TerminableStream (..),
-    {- * Vendors -} nil, singleton, list, map, concatMap, concat, while,
+    {- * Vendors -} nil, singleton, list, map, concatMap, concat, while, group,
     {- * Vendor composition -} append, concatMapVendor,
     {- * Clients -} all,
   )
@@ -12,12 +12,15 @@ import SupplyChain
 import Control.Applicative (pure)
 import Control.Monad ((>>=))
 import Data.Bool (Bool (..))
+import Data.Eq (Eq, (==))
 import Data.Function (($), id)
 import Data.Functor (Functor, (<$>))
 import Data.Functor.Const (Const (..))
 import Data.Kind (Type)
 import Data.Maybe (Maybe (..))
 import Data.Void (Void)
+import Numeric.Natural (Natural)
+import Prelude ((+))
 
 
 data TerminableStream item response =
@@ -149,3 +152,25 @@ all = go
     go = order NextMaybe >>= \case
         Nothing -> pure []
         Just x -> (x :) <$> go
+
+
+group :: forall a action. Eq a => Functor action =>
+    Vendor (TerminableStream a) (TerminableStream (Natural, a)) action
+group = Vendor \NextMaybe ->
+    order NextMaybe >>= \case
+        Nothing -> pure $ Nothing :-> nil
+        Just x -> go 1 x
+  where
+    go :: Natural -> a -> Client (TerminableStream a) action
+        (
+            Supply
+                (TerminableStream a)
+                (TerminableStream (Natural, a))
+                action (Maybe (Natural, a))
+        )
+    go n x = order NextMaybe >>= \case
+        Nothing -> pure $ Just (n, x) :-> nil
+        Just y ->
+          if y == x
+          then go (n + 1) x
+          else pure $ Just (n, x) :-> Vendor \NextMaybe -> go 1 y
--- supply-chain/test-supply-chain/SupplyChain/Interface/TerminableStream/Spec.hs
+++ supply-chain/test-supply-chain/SupplyChain/Interface/TerminableStream/Spec.hs
@@ -3,6 +3,8 @@ module SupplyChain.Interface.TerminableStream.Spec (tests) where
 import Prelude (Maybe (..))
 
 import Control.Monad (replicateM)
+import Data.Char (Char)
+import Numeric.Natural (Natural)
 
 import Test.Tasty
 import Test.Tasty.HUnit ((@?=), testCase, Assertion)
@@ -16,6 +18,10 @@ tests = testGroup "TerminableStream"
     , testGroup "finiteConcat"
         [ testCase "with more" concatTest1
         , testCase "exhaustion" concatTest2
+        ]
+    , testGroup "group"
+        [ testCase "empty" groupEmpty
+        , testCase "letters" groupLetters
         ]
     ]
 
@@ -38,3 +44,15 @@ concatTest2 = eval supplyChain @?= result
     supplyChain = list ["a", "bc"] >-> concat
                     >-> replicateM 5 (order NextMaybe)
     result = [Just 'a', Just 'b', Just 'c', Nothing, Nothing]
+
+groupLetters :: Assertion
+groupLetters = eval supplyChain @?= result
+  where
+    supplyChain = (list "Hrmm..." >-> group >-> all)
+    result = [(1, 'H'), (1, 'r'), (2, 'm'), (3, '.')]
+
+groupEmpty :: Assertion
+groupEmpty = eval supplyChain @?= result
+  where
+    supplyChain = (nil >-> group >-> all)
+    result = [] :: [(Natural, Char)]
