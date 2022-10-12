0319a63fa3843a84ddf2e584008372009bb06ac3
Chris Martin <ch.martin@gmail.com>
2022-10-11 22:03:41 -0600

    takeChar test

--- step/test-step/Step/Toy/Spec.hs
+++ step/test-step/Step/Toy/Spec.hs
@@ -1,6 +1,6 @@
 module Step.Toy.Spec (tests) where
 
-import Step.Action (AtomicMove, SureQuery, Is, Any, Query, peekCharMaybe, takeCharMaybe, peekChar)
+import Step.Action (AtomicMove, SureQuery, Is, Any, Query, peekCharMaybe, takeCharMaybe, peekChar, takeChar)
 import Step.Toy (parse, parseSure, parseQuery, parseSureQuery)
 import Step.Chunk.ListLike (NonEmptyListLike, genChunks, fold)
 
@@ -48,6 +48,10 @@ tests =
       [ testPropertyNamed "empty" "prop_takeCharMaybe_empty" prop_takeCharMaybe_empty
       , testPropertyNamed "non-empty" "prop_takeCharMaybe_nonEmpty" prop_takeCharMaybe_nonEmpty
       ]
+    , testGroup "takeChar"
+      [ testPropertyNamed "empty" "prop_takeChar_empty" prop_takeChar_empty
+      , testPropertyNamed "non-empty" "prop_takeChar_nonEmpty" prop_takeChar_nonEmpty
+      ]
     ]
 
 
@@ -72,32 +76,34 @@ testPure parser input expectedResult expectedRemainder = do
     let (either (\() -> Nothing) Just -> result :: Maybe a, remainder :: [T]) = parse parser chunkedInput
     (result, fold remainder) === (expectedResult, expectedRemainder)
 
-prop_peekChar_empty :: Property
 prop_peekChar_empty = withTests 1 $ property do
     parseQuery peekChar noInput === Left ()
 
-prop_peekChar_nonEmpty :: Property
 prop_peekChar_nonEmpty = property do
     x <- forAll Gen.lower
     xs <- forAll (Gen.text (Range.linear 0 3) Gen.lower)
     testPureQuery peekChar (Text.cons x xs) (Just x)
 
-prop_peekCharMaybe_empty :: Property
 prop_peekCharMaybe_empty = withTests 1 $ property do
     parseSureQuery peekCharMaybe noInput === Nothing
 
-prop_peekCharMaybe_nonEmpty :: Property
 prop_peekCharMaybe_nonEmpty = property do
     x <- forAll Gen.lower
     xs <- forAll (Gen.text (Range.linear 0 3) Gen.lower)
     testPureSureQuery peekCharMaybe (Text.cons x xs) (Just x)
 
-prop_takeCharMaybe_empty :: Property
 prop_takeCharMaybe_empty = withTests 1 $ property do
     parse takeCharMaybe [] === (Right Nothing :: Either () (Maybe Char), noInput)
 
-prop_takeCharMaybe_nonEmpty :: Property
 prop_takeCharMaybe_nonEmpty = property do
     x <- forAll Gen.lower
     xs <- forAll (Gen.text (Range.linear 0 3) Gen.lower)
     testPure takeCharMaybe (Text.cons x xs) (Just (Just x)) xs
+
+prop_takeChar_empty = withTests 1 $ property do
+    parse takeChar [] === (Left (), noInput)
+
+prop_takeChar_nonEmpty = property do
+    x <- forAll Gen.lower
+    xs <- forAll (Gen.text (Range.linear 0 3) Gen.lower)
+    testPure takeChar (Text.cons x xs) (Just x) xs
