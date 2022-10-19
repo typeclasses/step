85138dc12c0cc890feaed6476e2d9ebced1841cf
Chris Martin <ch.martin@gmail.com>
2022-10-19 00:34:14 -0600

    import cleanup

--- step/step/Step/Chunk/ListLike.hs
+++ step/step/Step/Chunk/ListLike.hs
@@ -1,36 +1,3 @@
 module Step.Chunk.ListLike (NonEmptyListLike) where
 
-import Step.Chunk hiding (concat)
-import Step.Chunk.ListLike.Core hiding (length, generalize)
-import qualified Step.Chunk as Chunk
-
-import Control.Applicative (pure, (<*>))
-import Data.Bool (Bool (..))
-import Data.Eq (Eq ((==)))
-import Data.Foldable (Foldable)
-import Data.Function (($), (&), (.), on)
-import Data.Functor ((<&>), fmap)
-import Data.Functor.Contravariant (Predicate (..), Equivalence (..))
-import Data.ListLike (ListLike)
-import Data.Maybe (Maybe (..), fromMaybe, maybe)
-import Data.Ord (Ord (compare), Ordering (..))
-import Data.Sequence (Seq (..))
-import Data.String (IsString (..))
-import GHC.Exts (IsList (..))
-import Hedgehog (Gen)
-import NatOptics.Positive.Unsafe (Positive (PositiveUnsafe))
-import Numeric.Natural (Natural)
-import Optics (preview, review)
-import Prelude (error)
-import Prelude (fromIntegral)
-import Text.Show (Show (showsPrec))
-import Data.Semigroup (Semigroup (..))
-
-import qualified Data.List.NonEmpty as NE
-import qualified Data.Foldable as Foldable
-import qualified Data.ListLike as LL
-import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
-import qualified NatOptics.Positive as Positive
-import qualified NatOptics.Positive.Math as Positive
-import qualified NatOptics.Signed as Signed
+import Step.Chunk.ListLike.Core
