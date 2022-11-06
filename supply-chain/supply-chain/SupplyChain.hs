2e8bb4e5277cf9c8cf00258a09eacff57d37afb9
Chris Martin <ch.martin@gmail.com>
2022-11-06 00:24:05 -0600

    clean up SupplyChain more

--- supply-chain/supply-chain/SupplyChain.hs
+++ supply-chain/supply-chain/SupplyChain.hs
@@ -3,20 +3,19 @@ module SupplyChain
 
     {- * Job -} {- $job -} Job, order, perform, run, eval,
     {- * Vendor -} {- $vendor -} Vendor (Vendor, handle), Supply (Supply),
-    {- * Connect -} {- $connect -} vendorToJob, vendorToVendor, Connect (..),
+    {- * Connect -} {- $connect -} vendorToJob, vendorToVendor, Connect ((>->)),
     {- * Interface -} Interface, NoInterface,
     {- * Action -} Action, NoAction,
 
   )
   where
 
-import SupplyChain.Connect (Connect (..), vendorToJob, vendorToVendor)
-
-import SupplyChain.Core.Job (Job, order, perform, run, eval)
-import SupplyChain.Core.Kinds (Type, Action, Interface)
-import SupplyChain.Core.Nil (NoAction, NoInterface)
-import SupplyChain.Core.Supply (Supply (..))
-import SupplyChain.Core.Vendor (Vendor (..))
+import SupplyChain.Connect (Connect ((>->)), vendorToJob, vendorToVendor)
+import SupplyChain.Job (Job, order, perform, run, eval)
+import SupplyChain.Kinds (Action, Interface)
+import SupplyChain.Nil (NoAction, NoInterface)
+import SupplyChain.Supply (Supply (Supply))
+import SupplyChain.Vendor (Vendor (Vendor, handle))
 
 -- $job
 -- See also: "SupplyChain.Job"
