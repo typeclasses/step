ba2a1bc048bc5d1c9d3a57d238c4d6416a42d19a
Chris Martin <ch.martin@gmail.com>
2022-11-07 09:27:22 -0700

    Revert "(unfinished) consider putting Vendor at the forefront"
    This reverts commit 1df1fc5498b2bf55b61188178b87a6dbfb857c41.

--- supply-chain/supply-chain/SupplyChain/Connect.hs
+++ supply-chain/supply-chain/SupplyChain/Connect.hs
@@ -1,16 +1,26 @@
-module SupplyChain.Connect ((>->), (>+>), (:+) (..)) where
+module SupplyChain.Connect (Connect (..),
+    vendorToJob, vendorToVendor, vendorToJob') where
 
 import SupplyChain.Core.Connect (vendorToJob, vendorToJob', vendorToVendor)
-import SupplyChain.Core.Supply (Supply)
 import SupplyChain.Core.Types (Vendor, Job)
 
-import Data.Functor.Const (Const)
+class Connect up down action client result | up client -> result,
+    client -> down action, result -> up action
+  where
+    {-| Generalizes 'vendorToJob' and 'vendorToVendor'
 
-(>->) :: Vendor up middle action -> Vendor middle down action -> Vendor up down action
-(>->) = vendorToVendor
+        This operation is associative; if @a@ and @b@ are vendors
+        and @c@ is a job, then @(a >-> b) >-> c@ is the same supply
+        chain as @a >-> (b >-> c)@.
+    -}
+    (>->) :: Vendor up down action -> client -> result
 
-(>+>) :: Vendor up middle action -> Vendor middle down action
-    -> Vendor up (down :+ Vendor up down action) action
-(>+>) = _
+instance Connect up down action
+    (Job down action product) (Job up action product)
+  where
+    (>->) = vendorToJob
 
-data (:+) i extra product where (:+) :: product ~ (p, extra) => i p -> (:+) i extra product
+instance Connect up middle action
+    (Vendor middle down action) (Vendor up down action)
+  where
+    (>->) = vendorToVendor
