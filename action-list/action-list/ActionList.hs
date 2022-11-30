2ac713a232c0070587d6675363028fa30435d939
Chris Martin <ch.martin@gmail.com>
2022-11-29 23:08:58 -0700

    action-list: shorten names

--- action-list/action-list/ActionList.hs
+++ action-list/action-list/ActionList.hs
@@ -1,6 +1,6 @@
 module ActionList
   (
-    {- * Type         -}  ActionList (..),
+    {- * Type         -}  List (..),
     {- * Introduction -}  fromList, perform,
     {- * Elimination  -}  toList, run,
     {- * Single step  -}  next,
@@ -29,72 +29,72 @@ import Data.Traversable (sequence)
 import Data.Void (Void)
 import Numeric.Natural (Natural)
 
-newtype ActionList m a =
-  VendorActionList
-    { actionListVendor :: Vendor (Const Void) (Unit (Maybe a)) m
+newtype List m a =
+  List
+    { vendor :: Vendor (Const Void) (Unit (Maybe a)) m
     }
 
 -- | @(xs '<>' ys) = ('append' xs ys)@
-instance Semigroup (ActionList m a)
+instance Semigroup (List m a)
   where
     (<>) = append
 
 -- | @'mempty' = 'empty'@
-instance Monoid (ActionList m a)
+instance Monoid (List m a)
   where
     mempty = empty
 
 -- | @('fmap' f xs) = ('map' f xs)@
-instance Functor (ActionList m)
+instance Functor (List m)
   where
-    fmap f (VendorActionList v) =
-        VendorActionList (v >-> Internal.map f)
+    fmap f (List v) =
+        List (v >-> Internal.map f)
 
-instance Applicative (ActionList m)
+instance Applicative (List m)
   where
-    pure = VendorActionList . Internal.singleton
+    pure = List . Internal.singleton
     (<*>) = ap
 
 -- | @(xs '>>=' f) = ('concatMap' f xs)@
-instance Monad (ActionList m)
+instance Monad (List m)
   where
     xs >>= f = concatMap f xs
 
-empty :: forall m a. ActionList m a
-empty = VendorActionList Internal.nil
+empty :: forall m a. List m a
+empty = List Internal.nil
 
-append :: forall m a.  ActionList m a -> ActionList m a -> ActionList m a
-append a b = VendorActionList $
-    Internal.append (actionListVendor a) (actionListVendor b)
+append :: forall m a.  List m a -> List m a -> List m a
+append a b = List $
+    Internal.append (vendor a) (vendor b)
 
-map :: forall m a b. (a -> b) -> ActionList m a -> ActionList m b
-map f xs = VendorActionList (actionListVendor xs >-> Internal.map f)
+map :: forall m a b. (a -> b) -> List m a -> List m b
+map f xs = List (vendor xs >-> Internal.map f)
 
-concatMap :: forall m a b. (a -> ActionList m b) -> ActionList m a -> ActionList m b
-concatMap f xs = VendorActionList $
-    actionListVendor xs >-> Internal.concatMapVendor (actionListVendor . f)
+concatMap :: forall m a b. (a -> List m b) -> List m a -> List m b
+concatMap f xs = List $
+    vendor xs >-> Internal.concatMapVendor (vendor . f)
 
--- | Converts an ordinary list into an 'ActionList'
-fromList :: forall m a. [a] -> ActionList m a
-fromList = VendorActionList . Internal.list
+-- | Converts an ordinary list into an 'List'
+fromList :: forall m a. [a] -> List m a
+fromList = List . Internal.list
 
 -- | A singleton list where the item is obtained by performing an action
-perform :: forall m a. m a -> ActionList m a
-perform = VendorActionList . Internal.actionSingleton
+perform :: forall m a. m a -> List m a
+perform = List . Internal.actionSingleton
 
--- | Converts an 'ActionList' into an ordinary list
-toList :: forall a. ActionList (Const Void) a -> [a]
-toList xs = Job.eval $ actionListVendor xs >- Internal.all
+-- | Converts an 'List' into an ordinary list
+toList :: forall a. List (Const Void) a -> [a]
+toList xs = Job.eval $ vendor xs >- Internal.all
 
--- | Converts an 'ActionList' into an action that returns all the items at once
-run :: forall m a. Monad m => ActionList m a -> m [a]
-run xs = Job.run $ actionListVendor xs >- Internal.all
+-- | Converts an 'List' into an action that returns all the items at once
+run :: forall m a. Monad m => List m a -> m [a]
+run xs = Job.run $ vendor xs >- Internal.all
 
-next :: forall m a. Monad m => ActionList m a -> m (Maybe (a, ActionList m a))
-next xs = fmap @m f $ Vendor.run (actionListVendor xs) Unit
+next :: forall m a. Monad m => List m a -> m (Maybe (a, List m a))
+next xs = fmap @m f $ Vendor.run (vendor xs) Unit
   where
-    f :: Referral (Const Void) (Unit (Maybe a)) m (Maybe a) -> Maybe (a, ActionList m a)
-    f s = s & sequence & fmap @Maybe (\(Referral x v) -> (x, VendorActionList v))
+    f :: Referral (Const Void) (Unit (Maybe a)) m (Maybe a) -> Maybe (a, List m a)
+    f s = s & sequence & fmap @Maybe (\(Referral x v) -> (x, List v))
 
 {-| Removes consecutive duplicate items, and yields each item along
     with the size of the repetition.
@@ -107,9 +107,9 @@ next xs = fmap @m f $ Vendor.run (actionListVendor xs) Unit
 
     @[(1, \'H'), (1, \'r'), (2, \'m'), (3, \'.')]@
 -}
-group :: forall m a. Eq a => ActionList m a -> ActionList m (Natural, a)
-group = VendorActionList . (>-> Internal.group) . actionListVendor
+group :: forall m a. Eq a => List m a -> List m (Natural, a)
+group = List . (>-> Internal.group) . vendor
 
 -- | The longest prefix matching the predicate
-takeWhile :: forall m a. (a -> Bool) -> ActionList m a -> ActionList m a
-takeWhile ok = VendorActionList . (>-> Internal.while ok) . actionListVendor
+takeWhile :: forall m a. (a -> Bool) -> List m a -> List m a
+takeWhile ok = List . (>-> Internal.while ok) . vendor
