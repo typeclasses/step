944542c2713d01a8d001417c23327d048708be98
Chris Martin <ch.martin@gmail.com>
2022-11-29 23:39:10 -0700

    define List with a parameter up instead of Const Void

--- action-list/action-list/ActionList.hs
+++ action-list/action-list/ActionList.hs
@@ -19,8 +19,8 @@ import Control.Applicative (Applicative, pure, (<*>))
 import Control.Monad (Monad, (>>=), ap)
 import Data.Bool (Bool)
 import Data.Eq (Eq)
-import Data.Function ((.), ($), (&))
-import Data.Functor (Functor, fmap)
+import Data.Function (($), (&))
+import Data.Functor (Functor, fmap, (<&>))
 import Data.Functor.Const (Const)
 import Data.Maybe (Maybe)
 import Data.Monoid (Monoid, mempty)
@@ -31,7 +31,7 @@ import Numeric.Natural (Natural)
 
 newtype List m a =
   List
-    { vendor :: Vendor (Const Void) (Unit (Maybe a)) m
+    { vendor :: forall up. Vendor up (Unit (Maybe a)) m
     }
 
 -- | @(xs '<>' ys) = ('append' xs ys)@
@@ -52,7 +52,7 @@ instance Functor (List m)
 
 instance Applicative (List m)
   where
-    pure = List . Internal.singleton
+    pure x = List (Internal.singleton x)
     (<*>) = ap
 
 -- | @(xs '>>=' f) = ('concatMap' f xs)@
@@ -72,15 +72,15 @@ map f xs = List (vendor xs >-> Internal.map f)
 
 concatMap :: forall m a b. (a -> List m b) -> List m a -> List m b
 concatMap f xs = List $
-    vendor xs >-> Internal.concatMapVendor (vendor . f)
+    vendor xs >-> Internal.concatMapVendor (\x -> vendor (f x))
 
 -- | Converts an ordinary list into an 'List'
 fromList :: forall m a. [a] -> List m a
-fromList = List . Internal.list
+fromList x = List (Internal.list x)
 
 -- | A singleton list where the item is obtained by performing an action
 perform :: forall m a. m a -> List m a
-perform = List . Internal.actionSingleton
+perform x = List (Internal.actionSingleton x)
 
 -- | Converts an 'List' into an ordinary list
 toList :: forall a. List (Const Void) a -> [a]
@@ -91,10 +91,8 @@ run :: forall m a. Monad m => List m a -> m [a]
 run xs = Job.run $ vendor xs >- Internal.all
 
 next :: forall m a. Monad m => List m a -> m (Maybe (a, List m a))
-next xs = fmap @m f $ Vendor.run (vendor xs) Unit
-  where
-    f :: Referral (Const Void) (Unit (Maybe a)) m (Maybe a) -> Maybe (a, List m a)
-    f s = s & sequence & fmap @Maybe (\(Referral x v) -> (x, List v))
+next xs = Vendor.run (vendor xs) Unit <&> \s ->
+    sequence s & fmap @Maybe (\(Referral x v') -> (x, List (Vendor.absurd >-> v')))
 
 {-| Removes consecutive duplicate items, and yields each item along
     with the size of the repetition.
@@ -108,8 +106,8 @@ next xs = fmap @m f $ Vendor.run (vendor xs) Unit
     @[(1, \'H'), (1, \'r'), (2, \'m'), (3, \'.')]@
 -}
 group :: forall m a. Eq a => List m a -> List m (Natural, a)
-group = List . (>-> Internal.group) . vendor
+group x = List (vendor x >-> Internal.group)
 
 -- | The longest prefix matching the predicate
 takeWhile :: forall m a. (a -> Bool) -> List m a -> List m a
-takeWhile ok = List . (>-> Internal.while ok) . vendor
+takeWhile ok x = List (vendor x >-> Internal.while ok)
