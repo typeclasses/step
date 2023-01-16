ffe56d3db46c3566fa5c5c4f24dc52dac4240557
Chris Martin <ch.martin@gmail.com>
2023-01-16 15:48:14 -0700

    add Cursor to the TerminableStream class

--- supply-cursor/supply-cursor/Cursor/Interface/Type.hs
+++ supply-cursor/supply-cursor/Cursor/Interface/Type.hs
@@ -1,15 +1,18 @@
 module Cursor.Interface.Type where
 
-import Data.Maybe (Maybe)
 import Integer (Positive)
+import Next.Interface (Next (..), Step (..), TerminableStream (..))
 
 data Mode = R | RW
 
-data Cursor (mode :: Mode) chunk product =
-    ( product ~ Maybe chunk             ) => NextChunk
+data Cursor (mode :: Mode) block product =
+    ( product ~ Step block             ) => NextBlock
   | ( product ~ ()                      ) => Reset
   | ( product ~ Advancement, mode ~ 'RW ) => Commit Positive
 
 data Advancement =
     AdvanceSuccess
   | YouCanNotAdvance{ shortfall :: Positive }
+
+instance TerminableStream block (Cursor mode block) where
+    liftNext Next = NextBlock
