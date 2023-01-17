231dd9d85c85b53c0e79ee14500e2cfc798c1e10
Chris Martin <ch.martin@gmail.com>
2023-01-17 00:48:39 -0700

    polymorphic Cursor functiosn

--- supply-cursor/supply-cursor/Cursor/Interface/Class.hs
+++ supply-cursor/supply-cursor/Cursor/Interface/Class.hs
@@ -1,9 +1,31 @@
-module Cursor.Interface.Class where
+module Cursor.Interface.Class
+  (
+    IsCursor (..),
+    commit, reset, next,
+  )
+  where
 
 import Cursor.Interface.Type
 
-class IsCursor (mode :: Mode) block interface where
+import Integer (Positive)
+import Next (Step, TerminableStream)
+
+import qualified Next
+
+class TerminableStream block interface =>
+    IsCursor (mode :: Mode) block interface
+    | interface -> mode block
+  where
     liftCursor :: Cursor mode block product -> interface product
 
 instance IsCursor mode block (Cursor mode block) where
     liftCursor x = x
+
+commit :: IsCursor 'Write block up => Positive -> up Advancement
+commit x = liftCursor (Commit x)
+
+reset :: IsCursor mode block up => up ()
+reset = liftCursor Reset
+
+next :: IsCursor mode block up => up (Step block)
+next = Next.next
