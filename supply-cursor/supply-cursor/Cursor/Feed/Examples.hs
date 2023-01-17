9d07fee18d608fab3744ddd587d1693fd4dbe0a4
Chris Martin <ch.martin@gmail.com>
2023-01-17 01:32:14 -0700

    Cursor.Feed.Examples

--- supply-cursor/supply-cursor/Cursor/Feed/Examples.hs
+++ supply-cursor/supply-cursor/Cursor/Feed/Examples.hs
@@ -1,27 +1,65 @@
-module Cursor.Feed.Examples where
+module Cursor.Feed.Examples
+  (
+    produceFromState,
+    pipe,
+    pipeWithBufferState,
+    pipeWithBufferReporting,
+  )
+  where
 
 import Essentials
 import Cursor.Feed.Type
 import Cursor.Interface
 
-import Data.Sequence (Seq)
+import Block.Class (Block)
+import Block.Class (Drop (..), drop)
+import Control.Monad.State (MonadState)
+import Data.Sequence (Seq (..))
 import Integer (Positive)
 import Next (TerminableStream, Step (..))
-import Block.Class (Block)
-import SupplyChain (Job, Vendor (Vendor), order, Referral (Referral))
-import Data.Sequence (Seq (..))
-import Block.Class (Drop (..), drop)
+import Optics (Lens', use, assign)
+import SupplyChain (Job, Vendor (Vendor), order, Referral (Referral), perform, (>->))
 
+import qualified SupplyChain.Vendor as Vendor
 import qualified Data.Sequence as Seq
 import qualified Next
 
+{-| In-memory cursor feed that obtains its input from state -}
+produceFromState :: forall state up action block.
+    Block block => MonadState state action =>
+    Lens' state (Seq block) -> FeedPlus up action 'Write block
+produceFromState buffer = Next.empty >-> pipeWithBufferState buffer
+
+{-| Turn a block producer into a cursor feed -}
+pipe :: forall block up action. Block block => TerminableStream block up =>
+    FeedPlus up action 'Write block
+pipe = pipeWithBufferReporting (\_ -> pure ()) Empty
+
+{-| Turn a block producer into a cursor feed, using state to store remainders
+
+A buffer stored in the 'MonadState' context, at a position identified by the
+given lens parameter, holds any input that has been read from the unbuffered
+stream but has not yet been committed. The remaining input, then, consists of
+anything that is in the buffer, followed by anything that is yet to be obtained
+from the unbuffered stream. -}
+pipeWithBufferState :: forall state up action block.
+    Block block => MonadState state action => TerminableStream block up =>
+    Lens' state (Seq block) -> Vendor up (Cursor 'Write block) action
+pipeWithBufferState buffer = Vendor \request -> do
+    b <- perform $ use buffer
+    Vendor.handle (pipeWithBufferReporting report b) request
+  where
+    report b = perform (assign buffer b)
+
 data DoubleBuffer c = DoubleBuffer{ commitBuffer :: Seq c, viewBuffer :: Seq c }
 
-reportedBuffer :: forall block up action. Block block => TerminableStream block up =>
+{-| Turn a block producer into a cursor feed, with manual handling of remainders -}
+pipeWithBufferReporting :: forall block up action.
+    Block block => TerminableStream block up =>
     (Seq block -> Job up action ()) -- ^ Called whenever the buffer changes
     -> Seq block -- ^ Initial buffer
     -> FeedPlus up action 'Write block
-reportedBuffer report b = go (DoubleBuffer b b)
+pipeWithBufferReporting report b = go (DoubleBuffer b b)
   where
     go s = Vendor \case
         Reset -> pure $ Referral () $ go s{ viewBuffer = commitBuffer s }
@@ -48,7 +86,3 @@ reportedBuffer report b = go (DoubleBuffer b b)
             Item x -> report com *> handleCommit s{ commitBuffer = com, viewBuffer = viewBuffer s :|> x } n
               where
                 com = Seq.singleton x
-
-privateBuffer :: forall block up action. Block block => TerminableStream block up =>
-    FeedPlus up action 'Write block
-privateBuffer = reportedBuffer (\_ -> pure ()) Empty
