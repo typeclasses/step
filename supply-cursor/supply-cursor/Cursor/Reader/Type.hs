afc93b8e679f9a8f4cec6ed201975cc8131f720c
Chris Martin <ch.martin@gmail.com>
2023-01-18 01:25:05 -0700

    reader name

--- supply-cursor/supply-cursor/Cursor/Reader/Type.hs
+++ supply-cursor/supply-cursor/Cursor/Reader/Type.hs
@@ -3,9 +3,9 @@ module Cursor.Reader.Type where
 import Cursor.Interface
 import SupplyChain
 
-type Consumer action mode block product =
+type Reader action mode block product =
     Job (Cursor mode block) action product
 
-type ConsumerPlus up action mode block product =
+type ReaderPlus up action mode block product =
     IsCursor mode block up =>
         Job up action product
