3f97c956c481fb99f8577f2fee41b8fe603d672f
Chris Martin <ch.martin@gmail.com>
2023-01-18 13:50:27 -0700

    note

--- block-class/block-class/Block/Class/ItemEquivalence.hs
+++ block-class/block-class/Block/Class/ItemEquivalence.hs
@@ -10,7 +10,10 @@ import Prelude (error)
 
 It must be the case that blocks /a/ and /b/ are equivalent iff the length
 of /a/ and /b/ is /l/ and /a[i] ~ b[i]/ for all /i = [1 .. l]/ according
-to the character equivalence. -}
+to the character equivalence.
+
+The point here is that we want to work with efficiently packed string types
+like @Text@ but still be able to reason about them as if they were @[Char]@. -}
 newtype BlockCharacterEquivalence c =
     BlockCharacterEquivalence{ blocksEquivalent :: c -> c -> Bool }
 
