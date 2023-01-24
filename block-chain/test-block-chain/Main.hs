723629a9c42d54f6d2366a1708af375887b85c45
Chris Martin <ch.martin@gmail.com>
2023-01-24 01:04:09 -0700

    stub block chain test suite

---
+++ block-chain/test-block-chain/Main.hs
@@ -0,0 +1,19 @@
+import Essentials
+
+import Block.Gen (genBlocks)
+import Data.Text (Text)
+import Test.Hspec (describe, it, shouldBe, hspec)
+import Test.Hspec.Hedgehog (hedgehog)
+import Hedgehog (forAll)
+import Block.Text (Text1)
+
+main = hspec do
+
+    describe "HTTP example" do
+
+        it "Request line" $ hedgehog do
+            let input :: Text = "GET /hello.txt HTTP/1.1"
+                reader = _
+            chunkedInput :: [Text1] <- forAll $ genBlocks input
+            _
+            -- runReader reader chunkedInput `shouldBe` ("GET", ["hello.txt"], ['1', '1'])
