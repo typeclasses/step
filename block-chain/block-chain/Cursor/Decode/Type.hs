e3ff61a8dc2116e1f3bd60fbace03f5be6a3f0a5
Chris Martin <ch.martin@gmail.com>
2023-01-29 01:17:27 -0700

    develop Decode using Seq1

--- block-chain/block-chain/Cursor/Decode/Type.hs
+++ block-chain/block-chain/Cursor/Decode/Type.hs
@@ -1,8 +1,15 @@
 module Cursor.Decode.Type where
 
-
+import Data.Sequence (Seq)
+import Block.Sequence (Seq1)
 
 newtype Decode block1 block2 =
-    Decode ([block1] -> DecodeResult block1 block2)
+    Decode (Seq1 block1 -> DecodeResult block1 block2)
 
-data DecodeResult block1 block2
+data DecodeResult block1 block2 = DecodeResult
+    { decoded :: Seq block2
+    , remainder :: Seq block1
+    , liveness :: Liveness
+    }
+
+data Liveness = Dead | Alive
