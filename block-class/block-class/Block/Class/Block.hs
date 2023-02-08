7f75d7100a10348198af64973b3d0b6ae3e74875
Chris Martin <ch.martin@gmail.com>
2023-02-08 00:01:39 -0700

    move Search types

--- block-class/block-class/Block/Class/Block.hs
+++ block-class/block-class/Block/Class/Block.hs
@@ -1,34 +1,6 @@
 module Block.Class.Block where
 
-import Essentials
-
-import Integer (Positive)
-import Data.List.NonEmpty (NonEmpty, nonEmpty)
-import Block.Item (Item)
-import Block.Singleton.Class (Singleton)
-import Data.Semigroup (sconcat)
 import Block.Positional.Class (Positional)
 import Block.Search.Class (Search)
 
-import qualified Data.Semigroup as Semigroup
-
 class (Positional xs, Search xs) => Block xs
-
-data Division x c =
-    NoDivision | Division (Maybe c) x (Maybe c)
-    deriving stock (Eq, Ord, Show, Functor)
-
-data Span c =
-    SpanAll
-  | SpanNone
-  | SpanPart
-      { spannedPart :: c
-      , spanRemainder :: c
-      }
-  deriving stock (Eq, Ord, Show, Functor)
-
-data While c =
-    WhileNone
-  | WhilePrefix c
-  | WhileAll
-  deriving stock (Eq, Ord, Show, Functor)
--- block-class/block-class/Block/Search/Types.hs
+++ block-class/block-class/Block/Search/Types.hs
@@ -1,1 +1,22 @@
 module Block.Search.Types where
+
+import Essentials
+
+data Division x c =
+    NoDivision | Division (Maybe c) x (Maybe c)
+    deriving stock (Eq, Ord, Show, Functor)
+
+data Span c =
+    SpanAll
+  | SpanNone
+  | SpanPart
+      { spannedPart :: c
+      , spanRemainder :: c
+      }
+  deriving stock (Eq, Ord, Show, Functor)
+
+data While c =
+    WhileNone
+  | WhilePrefix c
+  | WhileAll
+  deriving stock (Eq, Ord, Show, Functor)
