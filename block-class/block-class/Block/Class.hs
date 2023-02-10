3c79fb7a26b2b7806c8388f567dae0affa50384f
Chris Martin <ch.martin@gmail.com>
2023-02-09 23:23:10 -0700

    module Block.Classes

--- block-class/block-class/Block/Class.hs
+++
@@ -1,9 +0,0 @@
-module Block.Class
-  (
-    module Block.Class.Block,
-    module Block.Class.Trivializable,
-  )
-  where
-
-import Block.Class.Block
-import Block.Class.Trivializable
---
+++ block-class/block-class/Block/Classes.hs
@@ -0,0 +1,11 @@
+module Block.Classes
+  (
+    Singleton, Positional, Search, Block, Trivializable,
+  )
+  where
+
+import Block.Class.Block
+import Block.Class.Trivializable
+import Block.Singleton.Class
+import Block.Positional.Class
+import Block.Search.Class
