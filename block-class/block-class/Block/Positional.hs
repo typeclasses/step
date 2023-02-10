927cd122cee58f98491aca87ba2f968db1b9f23a
Chris Martin <ch.martin@gmail.com>
2023-02-10 16:19:04 -0700

    pull Positional definitions into top of group

--- block-class/block-class/Block/Positional.hs
+++ block-class/block-class/Block/Positional.hs
@@ -5,5 +5,50 @@ module Block.Positional
   )
   where
 
-import Block.Positional.Class
-import Block.Positional.Types
+import Essentials
+
+import Block.Singleton.Class
+
+import Integer (Positive)
+import Data.List.NonEmpty (NonEmpty (..))
+import Block.End (End)
+
+import qualified Integer.Positive as Positive
+
+class (Singleton xs) => Positional xs where
+
+    length :: xs -> Positive
+
+    split :: Amount -> xs -> Split xs
+
+    take :: Amount -> xs -> Take xs
+
+    drop :: Amount -> xs -> Drop xs
+
+instance Positional (NonEmpty xs) where
+
+    length = Positive.length
+
+data Amount = Amount End Positive
+
+{-| (Shortfall /n/) indicates that an operation which failed
+    would require a block operand to have /n/ more items. -}
+newtype Shortfall = Shortfall Positive
+    deriving stock (Eq, Ord, Show)
+
+data Split xs =
+    SplitInsufficient Shortfall
+  | Split xs xs
+  deriving stock (Eq, Ord, Show, Functor)
+
+data Drop xs =
+    DropAll
+  | DropInsufficient Shortfall
+  | DropPart xs
+  deriving stock (Eq, Ord, Show, Functor)
+
+data Take xs =
+    TakeAll
+  | TakeInsufficient Shortfall
+  | TakePart xs
+  deriving stock (Eq, Ord, Show, Functor)
--- block-class/block-class/Block/Positional/Types.hs
+++ block-class/block-class/Block/Positional/Types.hs
@@ -4,31 +4,4 @@ module Block.Positional.Types
   )
   where
 
-import Essentials
-
-import Integer (Positive)
-import Block.End (End)
-
-data Amount = Amount End Positive
-
-{-| (Shortfall /n/) indicates that an operation which failed
-    would require a block operand to have /n/ more items. -}
-newtype Shortfall = Shortfall Positive
-    deriving stock (Eq, Ord, Show)
-
-data Split xs =
-    SplitInsufficient Shortfall
-  | Split xs xs
-  deriving stock (Eq, Ord, Show, Functor)
-
-data Drop xs =
-    DropAll
-  | DropInsufficient Shortfall
-  | DropPart xs
-  deriving stock (Eq, Ord, Show, Functor)
-
-data Take xs =
-    TakeAll
-  | TakeInsufficient Shortfall
-  | TakePart xs
-  deriving stock (Eq, Ord, Show, Functor)
+import Block.Positional
