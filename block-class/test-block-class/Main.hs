0f7b0a55ee7712fd74d9ccb19d8ef0833a29a3ec
Chris Martin <ch.martin@gmail.com>
2023-02-12 17:52:55 -0700

    fuss with tests

--- block-class/test-block-class/Main.hs
+++ block-class/test-block-class/Main.hs
@@ -11,43 +11,84 @@ import Data.Char (isUpper, isLetter, isDigit, Char)
 import Text.Read (readMaybe)
 import Control.Monad (guard)
 import Data.Int (Int)
-import Hedgehog (forAll, (===))
+import Hedgehog (forAll, (===), Gen)
 import Prelude (error)
 
 import qualified Hedgehog.Gen as Gen
-
-ne :: [a] -> NonEmpty a
-ne (x : xs) = x :| xs
-ne [] = error "ne"
 
 main :: IO ()
 main = hspec do
 
     describe "span" do
         let str = ne "ABCdefGHI"
-        describe "part" do
-            it "1" $ span Front isUpper  str `shouldBe` SpanPart (ne "ABC") (ne "defGHI")
-            it "2" $ span Back  isUpper  str `shouldBe` SpanPart (ne "GHI") (ne "ABCdef")
-        it "all" $ span Back  isLetter str `shouldBe` SpanAll
-        it "none" $ span Back  isDigit  str `shouldBe` SpanNone
+
+        describe "SpanPart" do
+            it "1" $ span Front isUpper str `shouldBe`
+                SpanPart (ne "ABC") (ne "defGHI")
+            it "2" $ span Back isUpper str `shouldBe`
+                SpanPart (ne "GHI") (ne "ABCdef")
+
+        it "SpanAll" $ hedgehog do
+            end <- forAll Gen.enumBounded
+            span end isLetter str === SpanAll
+
+        it "SpanNone" $ hedgehog do
+            end <- forAll Gen.enumBounded
+            span end isDigit str === SpanNone
 
     describe "find" do
+
         describe "pivot in the middle" do
             let str = ne "abc1def2ghi"
             let digit x = readMaybe [x] :: Maybe Int
-            it "1" $ find Front digit str `shouldBe` Just (Pivot (nonEmpty "abc") (1 :: Int) (nonEmpty "def2ghi"))
-            it "2" $ find Back  digit str `shouldBe` Just (Pivot (nonEmpty "ghi") (2 :: Int) (nonEmpty "abc1def"))
+            it "1" $ find Front digit str `shouldBe`
+                Just (Pivot (nonEmpty "abc") 1 (nonEmpty "def2ghi"))
+            it "2" $ find Back digit str `shouldBe`
+                Just (Pivot (nonEmpty "ghi") 2 (nonEmpty "abc1def"))
+
         it "pivot on the end" $
-            find Front (\x -> Just x <* guard (isLetter x)) (ne "abc") `shouldBe` Just (Pivot Nothing 'a' (nonEmpty "bc"))
+            find Front (\x -> Just x <* guard (isLetter x)) (ne "abc")
+            `shouldBe` Just (Pivot Nothing 'a' (nonEmpty "bc"))
+
         it "no pivot" $ hedgehog do
-            end <- forAll (Gen.element [Front, Back])
-            str <- forAll (Gen.element [ne "a", ne "ab", ne "abc"])
-            find end (\_ -> Nothing) str === (Nothing :: Maybe (Pivot () (NonEmpty Char)))
+            end <- forAll Gen.enumBounded
+            str <- forAll genText
+            let result :: Maybe (Pivot () (NonEmpty Char))
+                result = find end (\_ -> Nothing) str
+            result === Nothing
 
     describe "biPrefix" do
-        it "First" $ biPrefix equality (ne "ab", ne "abcde") `shouldBe` IsPrefix First (ne "ab") (ne "cde")
-        it "Second" $ biPrefix equality (ne "abcde", ne "ab") `shouldBe` IsPrefix Second (ne "ab") (ne "cde")
-        it "Same" $ biPrefix equality (ne "abcd", ne "abcd") `shouldBe` Same
-        describe "NoPrefixRelation" do
-            it "1" $ biPrefix equality (ne "abc", ne "xabc") `shouldBe` NoPrefixRelation
-            it "2" $ biPrefix equality (ne "abc", ne "xyz") `shouldBe` NoPrefixRelation
+
+        it "(car, carpet) -> the first is a prefix" $
+            biPrefix equality (ne "car", ne "carpet")
+            `shouldBe` IsPrefix First (ne "car") (ne "pet")
+
+        it "(carpet, car) -> the second is a prefix" $
+            biPrefix equality (ne "carpet", ne "car")
+            `shouldBe` IsPrefix Second (ne "car") (ne "pet")
+
+        it "First/Second property" $ hedgehog do
+            a <- forAll genText
+            b <- forAll genText
+            biPrefix equality (a, a <> b) === IsPrefix First a b
+            biPrefix equality (a <> b, a) === IsPrefix Second a b
+
+        it "Same" $ hedgehog do
+            x <- forAll genText
+            biPrefix equality (x, x) === Same
+
+        it "NoPrefixRelation" $ hedgehog do
+            (a, b) <- forAll $ Gen.element
+              [ (ne "cat", ne "frog")
+              , (ne "pit", ne "pig")
+              , (ne "fish", ne "fit")
+              ]
+            pair <- forAll $ Gen.element [(a, b), (b, a)]
+            biPrefix equality pair === NoPrefixRelation
+
+ne :: [a] -> NonEmpty a
+ne (x : xs) = x :| xs
+ne [] = error "ne"
+
+genText :: Gen (NonEmpty Char)
+genText = Gen.element [ne "a", ne "ab", ne "abc"]
