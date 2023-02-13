7fabea0682909e942c1610ed5830ccea910c7d3b
Chris Martin <ch.martin@gmail.com>
2023-02-13 14:23:42 -0700

    doc

--- block-class/block-class/Block/Class/Shortfall.hs
+++ block-class/block-class/Block/Class/Shortfall.hs
@@ -4,7 +4,8 @@ import Essentials
 
 import Integer (Positive)
 
-{-| (Shortfall /n/) indicates that an operation which failed
-    would require a block operand to have /n/ more items. -}
+{-| @(Shortfall n)@ indicates that an operation which failed
+    would require a block operand to have @(n)@ more items to
+    succeed. -}
 newtype Shortfall = Shortfall Positive
     deriving stock (Eq, Ord, Show)
