ba0ea6135abb4145de04aacece00755f7160b8d9
Chris Martin <ch.martin@gmail.com>
2023-02-16 17:18:21 -0700

    simplify Pop

--- block-class/block-class/Block/Class/Singleton/Types.hs
+++ block-class/block-class/Block/Class/Singleton/Types.hs
@@ -3,7 +3,4 @@ module Block.Class.Singleton.Types where
 import Essentials
 
 data Pop x xs = Pop{ item :: x, remainder :: Maybe xs }
-
-deriving stock instance (Eq x, Eq xs) => Eq (Pop x xs)
-deriving stock instance (Ord x, Ord xs) => Ord (Pop x xs)
-deriving stock instance (Show x, Show xs) => Show (Pop x xs)
+    deriving stock (Eq, Ord, Show)
