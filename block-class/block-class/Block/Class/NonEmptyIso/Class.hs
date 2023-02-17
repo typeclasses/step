7af9a04a67f7539326e92c05a3da5910d6df5740
Chris Martin <ch.martin@gmail.com>
2023-02-17 16:33:57 -0700

    give NonEmptyIso a module group

---
+++ block-class/block-class/Block/Class/NonEmptyIso/Class.hs
@@ -0,0 +1,20 @@
+module Block.Class.NonEmptyIso.Class where
+
+import Essentials
+
+import Block.Class.End (End (..))
+import Data.List.NonEmpty (NonEmpty)
+
+import qualified Data.List.NonEmpty as NonEmpty
+
+class NonEmptyIso x xs | xs -> x where
+    toNonEmpty :: End -> xs -> NonEmpty x
+    fromNonEmpty :: End -> NonEmpty x -> xs
+
+instance NonEmptyIso x (NonEmpty x) where
+
+    toNonEmpty :: End -> NonEmpty x -> NonEmpty x
+    toNonEmpty = \case Front -> id; Back -> NonEmpty.reverse
+
+    fromNonEmpty :: End -> NonEmpty x -> NonEmpty x
+    fromNonEmpty = \case Front -> id; Back -> NonEmpty.reverse
