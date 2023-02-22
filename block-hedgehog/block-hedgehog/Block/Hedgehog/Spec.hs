d207b00358d8dcea5b44194b2dd4197702d85544
Chris Martin <ch.martin@gmail.com>
2023-02-22 12:21:31 -0700

    fix block-hedgehog

--- block-hedgehog/block-hedgehog/Block/Hedgehog/Spec.hs
+++ block-hedgehog/block-hedgehog/Block/Hedgehog/Spec.hs
@@ -1,15 +1,16 @@
 module Block.Hedgehog.Spec (spec) where
 
 import Essentials
+import Block.Class
 
 import Hedgehog (Gen)
 import Test.Hspec (Spec)
-import Block.Class (Block, Item)
 
 import qualified Block.Hedgehog.Spec.Singleton as Singleton
 
-spec :: forall xs x.
-    (Block xs, Show xs, Eq xs) =>
-    (Item xs ~ x, Show x, Eq x) =>
-    Gen xs -> Gen (Item xs) -> Spec
+spec :: forall x xs.
+    (Show x, Eq x) =>
+    (Show xs, Eq xs) =>
+    (Semigroup xs, Singleton x xs) =>
+    Gen x -> Gen xs -> Spec
 spec = Singleton.spec
--- block-hedgehog/block-hedgehog/Block/Hedgehog/Spec/Singleton.hs
+++ block-hedgehog/block-hedgehog/Block/Hedgehog/Spec/Singleton.hs
@@ -1,46 +1,47 @@
 module Block.Hedgehog.Spec.Singleton (spec) where
 
+import Block.Class
 import Essentials
 
 import Test.Hspec (Spec, describe, it)
 import Test.Hspec.Hedgehog (hedgehog)
 import Hedgehog (Gen, forAll, (===))
 
-import qualified Block.Class as Block
 import qualified Hedgehog.Gen as Gen
 
-spec :: forall xs x.
-    (Block.Singleton xs, Show xs, Eq xs) =>
-    (Block.Item xs ~ x, Show x, Eq x) =>
-    Gen xs -> Gen (Block.Item xs) -> Spec
-spec genBlock genItem = describe "Block.Singleton" do
+spec :: forall x xs.
+    (Show x, Eq x) =>
+    (Show xs, Eq xs) =>
+    (Semigroup xs, Singleton x xs) =>
+    Gen x -> Gen xs -> Spec
+spec genX genXs = describe "Singleton" do
 
     it "pop end . unpop end = id" $ hedgehog do
-        x <- forAll genItem
-        xs <- forAll (Gen.maybe genBlock)
+        x <- forAll genX
+        xs <- forAll (Gen.maybe genXs)
         end <- forAll Gen.enumBounded
-        let pop = Block.Pop x xs
-        (Block.pop end . Block.unpop end) pop === pop
+        let p = Pop x xs
+        (pop end . unpop end) p === p
 
     it "pop end (singleton x) = Pop x Nothing" $ hedgehog do
-        x <- forAll genItem
+        x <- forAll genX
         end <- forAll Gen.enumBounded
-        let xs :: xs = Block.singleton x
-        let pop = Block.pop end xs
-        pop === Block.Pop x Nothing
+        let xs :: xs = singleton x
+        let p = pop end xs
+        p === Pop x Nothing
 
     it "pop Front (singleton a <> singleton b) \
        \= Pop a (Just (singleton b))" $ hedgehog do
-        a <- forAll genItem
-        b <- forAll genItem
-        let xs :: xs = Block.singleton a <> Block.singleton b
-        let pop = Block.pop Block.Front xs
-        pop === Block.Pop a (Just (Block.singleton b))
+        a <- forAll genX
+        b <- forAll genX
+        let xs :: xs = singleton a <> singleton b
+        let p = pop Front xs
+        p === Pop a (Just (singleton b))
 
     it "pop Back (singleton a <> singleton b) \
        \= Pop b (Just (singleton a))" $ hedgehog do
-        a <- forAll genItem
-        b <- forAll genItem
-        let xs :: xs = Block.singleton a <> Block.singleton b
-        let pop = Block.pop Block.Back xs
-        pop === Block.Pop b (Just (Block.singleton a))
+        a <- forAll genX
+        b <- forAll genX
+        let xs :: xs = singleton a <> singleton b
+        let p = pop Back xs
+        p === Pop b (Just (singleton a))
