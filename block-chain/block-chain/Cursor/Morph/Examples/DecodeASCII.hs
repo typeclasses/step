ba30ade050c9612a6c536ff9ccb5175426c3752e
Chris Martin <ch.martin@gmail.com>
2023-03-03 00:04:18 -0700

    clean up ascii decode

--- block-chain/block-chain/Cursor/Morph/Examples/DecodeASCII.hs
+++ block-chain/block-chain/Cursor/Morph/Examples/DecodeASCII.hs
@@ -8,16 +8,14 @@ import Essentials
 import Cursor.Morph.Type
 
 import Block (ASCII1, ByteString1, End (..), Take (..), Shortfall (..))
-import Cursor.Advancement (Advancement (..), commitAlternative)
+import Cursor.Advancement (Advancement (..))
 import Cursor.Buffer (Buffer)
 import Cursor.Interface (Mode (..), Cursor (..), IsCursor, Step (..))
 import Control.Monad.State (StateT)
 import Control.Monad.Trans (lift)
-import Data.Function (flip)
 import Data.Sequence (Seq (..))
 import Data.Word (Word8)
 import Integer (Positive)
-import Miscellany (maybeAlternative)
 import Optics ((%), modifying)
 import SupplyChain (Vendor (Vendor), Job, Referral (Referral))
 
@@ -29,8 +27,6 @@ import qualified Cursor.Buffer as Buffer
 import qualified Cursor.Interface.Orders as Cursor
 import qualified Data.Maybe as Maybe
 import qualified Data.Sequence as Seq
-import qualified Integer
-import qualified Integer.Signed as Signed
 import qualified Optics
 
 data Vitality = Dead | Alive
@@ -48,73 +44,65 @@ uncommitted = buffer % Buffer.uncommitted
 unviewed    = buffer % Buffer.unviewed
 
 decodeAscii :: MorphPlus up action 'Write Word8 ByteString1 ASCII.Char ASCII1
-decodeAscii = decodeAscii' (S Buffer.empty Alive)
+decodeAscii = state (S Buffer.empty Alive) \case
+    Next     -> State.runStateT $ next
+    Commit n -> State.runStateT $ commit n
+    Reset    -> State.runStateT $ reset
+    Flush    -> State.runStateT $ reset
 
-decodeAscii' :: S -> MorphPlus up action 'Write Word8 ByteString1 ASCII.Char ASCII1
-decodeAscii' s = Vendor \case
-    Next -> next s
-    Commit n -> commit s n
-    Reset -> reset s
-    Flush -> reset s
+next :: IsCursor 'Write ByteString1 up => StateT S (Job up action) (Step ASCII1)
+next = Optics.use unviewed >>= \case
+    (Seq.:<|) x xs -> do
+        Optics.assign unviewed xs
+        pure $ Item x
+    Seq.Empty -> Optics.use vitality >>= \case
+        Dead -> pure End
+        Alive -> do
+            bufferMore
+            next
 
-next :: IsCursor 'Write ByteString1 up =>
-    S -> Job up action (Referral up (Cursor 'Write ASCII1) action (Step ASCII1))
-next s = do
-    (xm, s') <- flip State.runStateT s $
-        maybeAlternative (Optics.zoom buffer Buffer.next) nextFromUpstream
-    pure $ Referral (maybe End Item xm) (decodeAscii' s')
+commit :: IsCursor 'Write ByteString1 up => Positive
+    -> StateT S (Job up action) (Advancement () ())
+commit n = Optics.use uncommitted >>= \case
+    Seq.Empty -> Optics.use vitality >>= \case
+        Dead -> pure $ YouCanNotAdvance (Shortfall n) ()
+        Alive -> do
+            bufferMore
+            commit n
+    (Seq.:<|) x xs -> case Block.take Front n x of
+        TakeAll -> do
+            Optics.assign uncommitted xs
+            _ <- lift $ Cursor.commitPositive n
+            pure $ AdvanceSuccess ()
+        TakeInsufficient (Shortfall n') -> do
+            Optics.assign uncommitted xs
+            _ <- lift $ Cursor.commitPositive $ Block.length x
+            commit n'
+        TakePart{ takeRemainder = x' } -> do
+            Optics.assign uncommitted ((Seq.:<|) x' xs)
+            _ <- lift $ Cursor.commitPositive n
+            pure $ AdvanceSuccess ()
 
-nextFromUpstream :: IsCursor 'Write ByteString1 up =>
-    StateT S (Job up action) (Maybe ASCII1)
-nextFromUpstream = Optics.use vitality >>= \case
-    Dead -> pure Nothing
-    Alive -> lift Cursor.next >>= \case
-        End -> pure Nothing
-        Item x -> case Block.spanAscii Front x of
-            Nothing -> Optics.assign vitality Dead $> Nothing
-            Just (a, remainder) -> do
-                modifying uncommitted (:|> a)
-                Monad.when (Maybe.isJust remainder) $ Optics.assign vitality Dead
-                pure $ Just a
+state ::
+    s -- ^ Initial state
+    -> (forall response. down response -> s -> Job up action (response, s))
+    -> Vendor up down action
+state initial step = go initial
+  where
+    go s = Vendor
+        (\request -> step request s <&> \(response, s') ->
+                        Referral response (go s')
+        )
 
-commit :: IsCursor 'Write ByteString1 up => S -> Positive
-    -> Job up action (Referral up (Cursor 'Write ASCII1) action (Advancement () ()))
-commit s n = do
-    (r, s') <- flip State.runStateT s $
-        commitAlternative (Optics.zoom buffer . Buffer.commit) commitFromUpstream n
-    case r of
-        AdvanceSuccess _ -> void $ Cursor.commitPositive n
-        YouCanNotAdvance (Shortfall sh) _ -> case Integer.subtract n sh of
-            Signed.Plus n' -> void $ Cursor.commitPositive n'
-            _ -> pure ()
-    pure $ Referral r (decodeAscii' s')
+bufferMore :: IsCursor 'Write ByteString1 up => StateT S (Job up action) ()
+bufferMore = lift Cursor.next >>= \case
+    End -> Optics.assign vitality Dead
+    Item x -> case Block.spanAscii Front x of
+        Nothing -> Optics.assign vitality Dead
+        Just (a, remainder) -> do
+            Optics.modifying unviewed (:|> a)
+            Optics.modifying uncommitted (:|> a)
+            Monad.when (Maybe.isJust remainder) $ Optics.assign vitality Dead
 
-commitFromUpstream :: IsCursor 'Write ByteString1 up =>
-    Positive -> StateT S (Job up action) (Advancement () ())
-commitFromUpstream n = Optics.use vitality >>= \case
-    Dead -> pure $ YouCanNotAdvance (Shortfall n) ()
-    Alive -> lift Cursor.next >>= \case
-        End -> do
-            Optics.assign vitality Dead
-            pure $ YouCanNotAdvance (Shortfall n) ()
-        Item x -> case Block.spanAscii Front x of
-            Nothing -> do
-                Optics.assign vitality Dead
-                pure $ YouCanNotAdvance (Shortfall n) ()
-            Just (a, remainder) -> do
-                Optics.modifying unviewed (:|> a)
-                Optics.modifying uncommitted (:|> a)
-                Monad.when (Maybe.isJust remainder) $ Optics.assign vitality Dead
-                case Block.take Front n a of
-                    TakeAll -> pure $ AdvanceSuccess ()
-                    TakePart{ takeRemainder = x' } -> do
-                        Optics.assign uncommitted (Seq.singleton x')
-                        pure $ AdvanceSuccess ()
-                    TakeInsufficient (Shortfall n') -> commitFromUpstream n'
-
-reset :: IsCursor 'Write ByteString1 up => S
-    -> Job up action (Referral up (Cursor 'Write ASCII1) action ())
-reset s = do
-    let s' = s & Optics.over buffer \b ->
-                  b{ Buffer.bufferUnviewed = Buffer.bufferUncommitted b }
-    pure $ Referral () (decodeAscii' s')
+reset :: IsCursor 'Write ByteString1 up => StateT S (Job up action) ()
+reset = Optics.use uncommitted >>= Optics.assign unviewed
